13571f90716167a53c2647fdf18e90668a0f96c6
diegocr <dcasorran@gmail.com>
2014-02-08 22:33:33 +0100

    Initial Firefox fixes

--- secureboot.js
+++ secureboot.js
@@ -356,8 +356,8 @@ else
 			jsl.push({f:'js/fm.js', n: 'fm_js', j:1,w:12});
 			jsl.push({f:'js/filetypes.js', n: 'filetypes_js', j:1});
 			/* better download */
-			jsl.push({f:'js/queue.js', n: 'index', j:1,w:4});
 			jsl.push({f:'js/events.js', n: 'index', j:1,w:4});
+			jsl.push({f:'js/queue.js', n: 'index', j:1,w:4});
 			jsl.push({f:'js/downloadChrome.js', n: 'dl_js', j:1,w:3});
 			jsl.push({f:'js/downloadBlobBuilder.js', n: 'dl_js', j:1,w:3});
 			jsl.push({f:'js/downloadMemory.js', n: 'dl_js', j:1,w:3});

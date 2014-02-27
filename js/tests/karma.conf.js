8cd27b168a057a98a495e21b96815ffbcbd72869
Lyubomir Petrov <lpetrov@me.com>
2014-02-27 21:42:00 +0200

    coverage fix;

--- js/tests/karma.conf.js
+++ js/tests/karma.conf.js
@@ -44,7 +44,7 @@ module.exports = function(config) {
             "src/helpers/strophemocker.js",
             "src/helpers/utils.js",
              "../tests/src/unit/*.js",
-//             "../tests/src/integration/*.js",
+             "../tests/src/integration/*.js",
 
             { pattern: 'src/unit/fixtures/**/*.html', included: false, served: true }
 
@@ -56,10 +56,11 @@ module.exports = function(config) {
             // source files, that you wanna generate coverage for
             // do not include tests or libraries
             // (these files will be instrumented by Istanbul)
-            '../**/*.js': ['coverage']
+            '../chat/*.js': ['coverage'],
+            'src/helpers/*.js': ['coverage']
         },
 
-        browsers: ['PhantomJS_custom', 'PhantomJS'],
+//        browsers: ['PhantomJS_custom'],
 
         // you can define custom flags
         customLaunchers: {

c6083557d3a23a4674f10336df650396cce7e55f
Lyubomir Petrov <lpetrov@me.com>
2014-03-06 16:45:06 +0200

    added TCP and UDP turn servers (now temporarily using Alex's test servers);

--- js/chat/chat.js
+++ js/chat/chat.js
@@ -250,7 +250,11 @@ var MegaChat = function() {
     this.options = {
         'delaySendMessageIfRoomNotAvailableTimeout': 3000,
         'rtcSession': {
-            iceServers:[{url: 'stun:stun.l.google.com:19302'}]
+            iceServers:[
+                {url: 'stun:stun.l.google.com:19302'},
+                {url: 'turn:j100.server.lu:3478?transport=udp'},
+                {url: 'turn:j100.server.lu:3478?transport=tcp'}
+            ]
         },
         /**
          * Really simple plugin architecture

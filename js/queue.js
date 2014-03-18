d7e4a78025e107f2ec4020a344db45c9dd977997
César D. Rodas <crodas@php.net>
2014-03-18 14:32:23 -0300

    fix downloads

--- js/queue.js
+++ js/queue.js
@@ -42,10 +42,10 @@ var DEFAULT_CONCURRENCY = 4
 
 			queue._running.splice(id, 1);
 			queue.trigger('done', args)
-			queue._callback[args.__tid].apply(args, Array.prototype.slice.call(arguments, 0))
+			queue._callback[args.__tid](args, Array.prototype.slice.call(arguments, 0))
 			delete queue._callback[args.__tid];
 			if (args.__ondone) {
-				args.__ondone.apply(args, Array.prototype.slice.call(arguments, 0))
+				args.__ondone(args, Array.prototype.slice.call(arguments, 0))
 			}
 			setTimeout(function() {
 				queue.process();

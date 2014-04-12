1e6fabb8b0521f9d679b86fa213d69800a1ab972
César D. Rodas <crodas@php.net>
2014-04-12 17:29:44 -0400

    Upload works with new queue and new xhr manager
    TODO:
    - Add error handling at uploads
    - Recycle xhr and destroy them from time to time

--- js/xhr.js
+++ js/xhr.js
@@ -1,5 +1,6 @@
 (function(w) {
 
+// old {{{
 function MegaXHR() {
 	this.caller = null;
 	this.newXhr();
@@ -7,9 +8,15 @@ function MegaXHR() {
 
 MegaXHR.prototype.newXhr = function() {
 	this.xhr = new XMLHttpRequest;
-	this.xhr.onprogress = this.onprogress;
-	this.xhr.upload.progress = this.upload_progress;
-	this.xhr.onreadystatechange = this.onreadystatechange;
+	this.xhr.onprogress = bind(this.onprogress);
+	this.xhr.upload.progress = bind(this.upload_progress);
+	this.xhr.onerror = bind(this.onerror);
+	this.xhr.onabort = bind(this.onerror)
+	this.xhr.onreadystatechange = bind(this.onreadystatechange);
+}
+
+MegaXHR.prototype.open = function() {
+	this.xhr.open.apply(this.xhr, arguments);
 }
 
 MegaXHR.prototype.release = function() {
@@ -24,28 +31,28 @@ MegaXHR.prototype.attach = function(caller) {
 	return false;
 }
 
-MegaXHR.prototype.error = function() {
-	if (this.caller.error) {
-		this.caller.error(arguments, this.xhr);
+MegaXHR.prototype.onerror = function(args) {
+	if (this.caller.on_error) {
+		this.caller.on_error(args, this.xhr);
 		this.newXhr(); /* it failed, let's create a new xhr object */
 	}
 }
 
-MegaXHR.prototype.upload_progress = function() {
+MegaXHR.prototype.upload_progress = function(args) {
 	if (this.caller.on_upload_progress) {
-		this.caller.on_upload_progress(arguments, this.xhr);
+		this.caller.on_upload_progress(args, this.xhr);
 	}
 }
 
 MegaXHR.prototype.onprogress = function() {
 	if (this.caller.on_progress) {
-		this.caller.on_progress(arguments, this.xhr)
+		this.caller.on_progress(args, this.xhr)
 	}	
 }
 
-MegaXHR.prototype.onreadystatechange = function() {
+MegaXHR.prototype.onreadystatechange = function(args) {
 	if (this.xhr.readyState == this.DONE && this.caller.on_ready) {
-		this.caller.on_ready(arguments, this.xhr);
+		this.caller.on_ready(args, this.xhr);
 	}
 }
 
@@ -55,19 +62,37 @@ MegaXHR.prototype.send = function(data) {
 	}
 	this.xhr.send(data);
 }
+// }}}
 
 var _xhr_queue = [];
 
+function newXhr() {
+	var xhr = new XMLHttpRequest;
+	xhr.onreadystatechange = function() {
+		if (this.readyState == this.DONE && this.listener.on_ready) {
+			this.listener.on_ready(arguments, this);
+		}
+	}
+	xhr.onprogress = function() {
+		if (this.listener.on_progress) {
+			this.listener.on_progress(arguments, this)
+		}	
+	}
+
+	return xhr;
+}
+
 w.getXhr = function(object) {
 	for (var i = 0; i < _xhr_queue.length; i++) {
-		if (_xhr_queue[i].attach(object)) {
+		if (!_xhr_queue[i].listener) {
+			_xhr_queue[i].listener = object;
 			return _xhr_queue[i];
 		}
 	}
 
 	/* create a new xhr object */
-	var xhr = new MegaXHR;
-	xhr.attach(object);
+	var xhr = newXhr();
+	xhr.listener = object;
 
 	/* add it to the queue so we can recicle it */
 	_xhr_queue.push(xhr);
@@ -77,5 +102,3 @@ w.getXhr = function(object) {
 
 
 })(this);
-
-alert(getXhr());

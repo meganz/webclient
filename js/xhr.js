fbc9aea7d93f8a92443614844aa23643d5556376
César D. Rodas <crodas@php.net>
2014-04-12 18:02:34 -0400

    on error destroy xhr

--- js/xhr.js
+++ js/xhr.js
@@ -68,14 +68,40 @@ var _xhr_queue = [];
 
 function newXhr() {
 	var xhr = new XMLHttpRequest;
+	xhr.__id = parseInt(Math.random() * 0xffffffff);
 	xhr.onreadystatechange = function() {
 		if (this.readyState == this.DONE && this.listener.on_ready) {
 			this.listener.on_ready(arguments, this);
 		}
 	}
+	xhr.onerror = function() {
+		if (this.listener.on_error) {
+			this.listener.on_error(arguments, this, 'error');
+			this.listener = null; /* release */
+			for(var i = 0; i < _xhr_queue.length; i++) {
+				if (_xhr_queue[i].__id == this.__id) {
+					_xhr_queue.splice(i, 1);
+					break;
+				}
+			}
+		}
+	}
+	xhr.ontimeout = function() {
+		if (this.listener.on_error) {
+			this.listener.on_error(arguments, this, 'timeout');
+			this.listener = null; /* release */
+			for(var i = 0; i < _xhr_queue.length; i++) {
+				if (_xhr_queue[i].__id == this.__id) {
+					_xhr_queue.splice(i, 1);
+					break;
+				}
+			}
+		}
+	}
 	xhr.onprogress = function() {
 		if (this.listener.on_progress) {
 			this.listener.on_progress(arguments, this)
+			this.listener = null; /* we're done here, release this slot */
 		}	
 	}
 

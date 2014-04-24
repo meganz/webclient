79f46a357890313abab233b4da271612323cd3dd
andreidymovich <maximator2@mail.ru>
2014-04-24 14:19:21 +0300

    Flash for copyToClipboard button

--- html/js/backup.js
+++ html/js/backup.js
@@ -13,4 +13,5 @@ function init_backup()
 		var blob = new Blob([a32_to_base64(u_k)], {type: "text/plain;charset=utf-8"});
 		saveAs(blob, 'MEGA-KEY-BACKUP.txt');		
 	});
+	$('#clipboardbtn2').html(htmlentities(l[370]) + '<object data="OneClipboard.swf" id="clipboardswf1" type="application/x-shockwave-flash"  width="100%" height="26" allowscriptaccess="always"><param name="wmode" value="transparent"><param value="always" name="allowscriptaccess"><param value="all" name="allowNetworkin"><param name=FlashVars value="buttonclick=1" /></object>');
 }

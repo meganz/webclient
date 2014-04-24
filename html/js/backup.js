d7e79f690d3a6c7d6df72ffc1dab7dce29f0bba0
Bram van der Kolk <bk@mega.co.nz>
2014-04-24 23:36:18 +1200

    Copy to clipboard finalization (extension / regular)

--- html/js/backup.js
+++ html/js/backup.js
@@ -12,6 +12,30 @@ function init_backup()
 	{
 		var blob = new Blob([a32_to_base64(u_k)], {type: "text/plain;charset=utf-8"});
 		saveAs(blob, 'MEGA-KEY-BACKUP.txt');		
-	});
-	$('#clipboardbtn2').html(htmlentities(l[370]) + '<object data="OneClipboard.swf" id="clipboardswf1" type="application/x-shockwave-flash"  width="100%" height="26" allowscriptaccess="always"><param name="wmode" value="transparent"><param value="always" name="allowscriptaccess"><param value="all" name="allowNetworkin"><param name=FlashVars value="buttonclick=1" /></object>');
+	});	
+	
+	
+	if (is_extension)
+	{
+		$('.backup-input-button').unbind('mouseover');
+		$('.backup-input-button').bind('mouseover',function()
+		{
+			if (is_chrome_firefox) mozSetClipboard(a32_to_base64(u_k));
+			else
+			{
+				$('#backup_keyinput').select();
+				document.execCommand('copy');
+			}
+		});
+	}
+	else
+	{
+		$('.backup-input-button').html(htmlentities(l[63]) + '<object data="OneClipboard.swf" id="clipboardswf_backup" type="application/x-shockwave-flash"  width="100%" height="26" allowscriptaccess="always"><param name="wmode" value="transparent"><param value="always" name="allowscriptaccess"><param value="all" name="allowNetworkin"><param name=FlashVars value="buttonclick=1" /></object>');	
+		
+		$('.backup-input-button').unbind('mouseover');
+		$('.backup-input-button').bind('mouseover',function()
+		{
+			$('#clipboardswf_backup')[0].setclipboardtext(a32_to_base64(u_k));
+		});
+	}
 }

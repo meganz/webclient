204f4ebfb36eb030e7e63786f5d294710fef1874
César D. Rodas <crodas@php.net>
2014-05-28 17:02:22 -0400

    Add global speed report

--- js/functions.js
+++ js/functions.js
@@ -678,7 +678,8 @@ function CreateWorkers(url, message, size) {
 
 function percent_megatitle()
 {
-	var dl_r = 0, dl_t = 0, ul_r = 0, ul_t = 0, tp = $.transferprogress || {};
+	var dl_r = 0, dl_t = 0, ul_r = 0, ul_t = 0, tp = $.transferprogress || {}
+		, dl_s = 0, ul_s = 0
 	
 	for (var i in dl_queue)
 	{
@@ -694,6 +695,7 @@ function percent_megatitle()
 		{
 			dl_t += q.size || 0;
 		}
+		dl_s += ((q||{}).speed || 0) * 1000
 	}
 	
 	for (var i in ul_queue)
@@ -709,6 +711,7 @@ function percent_megatitle()
 		{
 			ul_t += ul_queue[i].size || 0;
 		}
+		ul_s += ((ul_queue[i]||{}).speed || 0)
 	}
 	if (dl_t) { dl_t += tp['dlc'] || 0; dl_r += tp['dlc'] || 0 }
 	if (ul_t) { ul_t += tp['ulc'] || 0; ul_r += tp['ulc'] || 0 }
@@ -732,6 +735,22 @@ function percent_megatitle()
 	{
 		t = '';
 		$.transferprogress = {};
+	}
+
+	if (dl_s > 0) {
+		$('.tranfer-download-indicator')
+			.text(bytesToSize(dl_s, 1) + "/s")
+			.addClass('active')
+	} else {
+		$('.tranfer-download-indicator').removeClass('active')
+	}
+
+	if (ul_s > 0) {
+		$('.tranfer-upload-indicator')
+			.text(bytesToSize(ul_s, 1) + "/s")
+			.addClass('active')
+	} else {
+		$('.tranfer-upload-indicator').removeClass('active')
 	}
 
 	$('.file-transfer-icon')

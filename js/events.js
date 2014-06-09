36a6a36bee9540a8e48f0474b58acf9997e65231
diegocr <dcasorran@gmail.com>
2014-06-09 07:05:56 +0200

    Some random tweaks here and there.

--- js/events.js
+++ js/events.js
@@ -5,6 +5,8 @@ function MegaEvents() {
 MegaEvents.prototype.trigger = function(name, args) {
 	if (!(this._events && this._events.hasOwnProperty(name))) return false;
 
+	if (d) console.log(' >>> Triggering ' + name, this._events[name].length, args );
+
 	args = args || []
 	var done = 0, evs = this._events[name];
 	for (var i in evs) {

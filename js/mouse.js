8dd654e7840de35e54529408b05d13bdaf410f5d
Artem S Vybornov <vybornov@gmail.com>
2014-06-20 03:02:13 +0400

    Review and fix @diegocr's entropy collector changes

--- js/mouse.js
+++ js/mouse.js
@@ -32,16 +32,31 @@ function mouseMoveEntropy(e)
 {
  lastactive = Date.now();
 
- if(mouseMoveSkip-- <= 0)
+ if( !u_storage.randseed && bioCounter < 40 )
+ {
+   // bioCounter is incremented once per 4 move events in avegare
+   // 40 * 4 = 160 first move events should provide at about 240 bits of entropy
+   // (conservative estimation is 1.5 bits of entropy per move event)
+   asmCrypto.random.seed( new Uint32Array([ e.screenX, e.screenY, timeByte() ]) );
+ }
+ else if(mouseMoveSkip-- <= 0)
  {
   var c = ((e.screenX << 4) | (e.screenY & 15));
+
   if (typeof arkanoid_entropy !== 'undefined') arkanoid_entropy();
   bioSeed[bioCounter++ & 255] ^= timeByte() | ( c << 8 );
   mouseMoveSkip = ( Math.random() * 8 ) | 0;
 
   if ( (bioCounter & 255) == 0 ) {
     if (d) console.log("Reseeding PRNG");
+
     asmCrypto.random.seed(bioSeed);
+
+    if ( u_storage ) {
+        var randseed = new Uint8Array(32);
+        asmCrypto.getRandomValues(randseed);
+        u_storage.randseed = base64urlencode( asmCrypto.bytes_to_string(randseed) );
+    }
   }
  }
 }
@@ -66,7 +81,11 @@ function eventsEnd()
 	
 function eventsCollect()
 {
- for(var i = 0xff ; i-- ; keyPressEntropy({keyCode:Date.now() & i}));
+ if (!d)
+    asmCrypto.random.skipSystemRNGWarning = true;
+
+ if ( u_storage.randseed )
+    asmCrypto.random.seed( asmCrypto.string_to_bytes( base64urldecode(u_storage.randseed) ) );
 
  if((document.implementation.hasFeature("Events", "2.0"))
   && document.addEventListener) // Document Object Model (DOM) 2 events

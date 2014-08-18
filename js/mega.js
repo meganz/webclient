e594ca5b040bef9cce9c1945791eeffd80917f13
Bram van der Kolk <bk@mega.co.nz>
2014-08-18 14:05:11 +1200

    Small fixes

--- js/mega.js
+++ js/mega.js
@@ -3054,12 +3054,13 @@ function execsc(ap)
 				{
 					u_nodekeys[a.n] = f.key;
 					if (f.name !== n.name)
-					{
+					{						
 						$('.grid-table.fm #' + n.h + ' .tranfer-filetype-txt').text(f.name);
 						$('.file-block#' + n.h + ' .file-block-title').text(f.name);
 						$('#treea_' + n.h + ' .nw-fm-tree-folder').text(f.name);
+						
+						//@@@Todo: reposition elements according to sorting (if sorted by name)						
 						if ($('#path_' + n.h).length > 0) newpath=1;
-						if (n.h == M.RootID) $('.fm-tree-header.cloud-drive-item span').text(f.name);
 					}
 					if (f.fav !== n.fav)
 					{

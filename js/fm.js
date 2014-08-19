828f0abffbe227582f14d992bbd6da4785b7b61e
Aleksandar Loncar <al@mega.co.nz>
2014-08-19 02:50:44 +0200

    Copy dialog work in progress

--- js/fm.js
+++ js/fm.js
@@ -1266,8 +1266,8 @@ function initContextUI()
 	$(c+'.copy-item').unbind('click');
 	$(c+'.copy-item').bind('click',function(event)
 	{
-		$.mctype='copy-cloud';
-		mcDialog();
+//		$.mctype='copy-cloud';
+//		mcDialog();
 	});
 
 	$(c+'.newfolder-item').unbind('click');

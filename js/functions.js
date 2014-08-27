2fb4f40072bd2580c8bff2616cf29c128c923ddb
diegocr <dcasorran@gmail.com>
2014-08-27 02:36:33 +0200

    #jscrashes fixes related to uploads

--- js/functions.js
+++ js/functions.js
@@ -621,11 +621,10 @@ function DEBUG() {
 function ASSERT(what, msg) {
 	if (!what)
 	{
+		var af = new Error('failed assertion; ' + msg);
+		Soon(function() { throw af; });
 		if (console.assert) console.assert(what, msg);
 		else console.error('FAILED ASSERTION', msg);
-		Soon(function() {
-			throw new Error('FAILED ASSERTION: ' + msg);
-		});
 	}
 }
 

e5fe3df3eea24f64b410b82e2fc462d01cf35dfb
Bram van der Kolk <bk@mega.co.nz>
2014-09-06 23:13:03 +1200

    Bug fix

--- html/js/sync.js
+++ html/js/sync.js
@@ -110,7 +110,6 @@ function init_sync()
 		});
 	},1000);
 	var pf = navigator.platform.toUpperCase();
-	pf = 'LINUX';
 	if (pf.indexOf('MAC')>=0) sync_switchOS('mac');
 	else if (pf.indexOf('LINUX')>=0) sync_switchOS('linux');
 	else sync_switchOS('windows');

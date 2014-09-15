260bd894c940c0392815a89fbdc7fb155d8e007c
andreidymovich <maximator2@mail.ru>
2014-09-15 10:38:39 +0300

    Tiny JS fix

--- js/mega.js
+++ js/mega.js
@@ -15,13 +15,13 @@ if (typeof loadingDialog == 'undefined')
 	loadingDialog.show = function()
 	{
 		$('.dark-overlay').show();
-		$('body').addClass('overlayed');
+		//$('body').addClass('overlayed');
 		$('.loading-spinner').show();
 	};
 	loadingDialog.hide = function()
 	{
 		$('.dark-overlay').hide();
-		$('body').removeClass('overlayed');
+		//$('body').removeClass('overlayed');
 		$('.loading-spinner').hide();
 	};
 }

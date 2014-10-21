8150af672d90a250d277f8520de93d5b4fbfc0c5
César D. Rodas <crodas@php.net>
2014-10-21 20:12:58 -0300

    Load blog from the CMS content

--- js/cms.js
+++ js/cms.js
@@ -9,15 +9,48 @@ console.log(pubkey)
 pubkey[0] = asmCrypto.hex_to_bytes(pubkey[0])
 pubkey[1] = asmCrypto.hex_to_bytes(pubkey[1])
 
-window.verify_cms_content = function(content, signature)
+function verify_cms_content(content, signature)
 {
     var hash = asmCrypto.SHA256.hex(content);
     signature = asmCrypto.base64_to_bytes(signature);
-    if (!asmCrypto.RSA_PSS_SHA256.verify(signature, hash, pubkey)) {
-        throw new Error('Signature missmatch');
-    }
+    return asmCrypto.RSA_PSS_SHA256.verify(signature, hash, pubkey);
+}
+
+function ab2str(buf) {
+	var	binaryString = '',
+		bytes = new Uint8Array(buf),
+		length = bytes.length;
+
+	for (var i = 0; i < length; i++) {
+		binaryString += String.fromCharCode(bytes[i]);
+	}
+
+	return binaryString;
+}
 
 
+window.cms_content = function(socket, ctx, buffer)
+{	
+	var data = {}
+		, headers = ['X-Mega-Authenticity', 'Content-Type']
+	for (var i in headers) {
+		try {
+			data[headers[i]] = socket.getResponseHeader(headers[i]);
+		} catch (e) {}
+	}
+
+	if (verify_cms_content(socket.response, data['X-Mega-Authenticity'])) {
+		var response = socket.response
+		switch (data['Content-Type']) {
+		case 'application/json':
+			response = JSON.parse(ab2str(response))
+			break;
+		}
+
+		ctx.callback(false, { buffer: response, mime: data['Content-Type']}, ctx);
+	} else {
+		ctx.callback(true, {} , ctx);
+	}
 }
 
 })(this, asmCrypto)
@@ -30,9 +63,7 @@ api_req = function(req, ctx, c)
 	if (req.a == "blob") {
 		var q = getxhr();
 		q.onload = function() {
-			var text = this.response;
-			verify_cms_content(text, this.getResponseHeader('X-Mega-Authenticity'));
-			alert("loaded object " + req.id + " from CMS");
+			cms_content(q, ctx);
 		};
 		q.responseType = 'arraybuffer';
 		q.open("GET", "/blobs.php?id=" + req.id);

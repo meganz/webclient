4e1cd3b5b9be6e613f9e9be8125be12130d1c960
diegocr <dcasorran@gmail.com>
2014-11-10 05:49:55 +0100

    Allow multiple + in email

--- js/functions.js
+++ js/functions.js
@@ -576,7 +576,7 @@ function makeid(len)
 
 function checkMail(email)
 {
-	email = email.replace('+','');
+	email = email.replace('+','','g');
 	var filter  = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 	if (filter.test(email)) return false;
 	else return true;

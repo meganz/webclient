c7828cccc161b365967dae6ca43e7bc618e56be0
Lyubomir Petrov <lpetrov@me.com>
2014-12-03 14:55:33 +1300

    updated the fabfile.py to make the "fab beta deploy" command work even when there is no "current_ver.txt" file;

--- dont-deploy/deployment/fabfile.py
+++ dont-deploy/deployment/fabfile.py
@@ -9,6 +9,10 @@ def beta():
 
 def deploy():
     with cd("/var/www"):
+        run("touch current_ver")
         run("cat current_ver>>~/deployer/last_ver")
+        run("git pull -u")
         run("git rev-parse HEAD>current_ver.txt")
+        with cd("logger"):
+            run("git pull -u")
         print "Latest version deployed: ", run("cat current_ver.txt")

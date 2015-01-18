2b5e7505e03d58a43a07f5225c5ab7b7ca44f6e8
Cesar Rodas <crodas@php.net>
2015-01-18 17:59:15 -0300

    Help from CMS is partially done
    Added listerner for notification on api server (Staging) as well on production

--- js/crypto.js
+++ js/crypto.js
@@ -1150,7 +1150,7 @@ function stopsc()
 // calls execsc() with server-client requests received
 function getsc(fm)
 {
-    api_req('sn=' + (maxaction||"") + '&ssl=1&e=Nc4AFJZK',{
+    api_req('sn=' + (maxaction||"") + '&ssl=1&e=aA0DxYzI&e=Nc4AFJZK',{
         fm : fm,
         callback : function __onGetSC(res,ctx)
         {

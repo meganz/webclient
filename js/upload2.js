4775b918cc2f2c426b3db32c04fe995ddac33b2f
diegocr <dcasorran@gmail.com>
2015-02-19 19:44:28 +0100

    #1751 - 'Skip identical files' only worked for the current folder

--- js/upload2.js
+++ js/upload2.js
@@ -67,6 +67,7 @@ function ul_deduplicate(File, identical) {
 		return ul_start(File)
 	} else if (M.h[uq.hash]) {
 		n = M.d[M.h[uq.hash][0]];
+		identical = n;
 	}
 	if (!n) return ul_start(File);
 	DEBUG(File.file.name, "ul_deduplicate", n)
@@ -984,8 +985,6 @@ function resetUploadDownload() {
 
 	Later(percent_megatitle);
 }
-
-if (localStorage.ul_skipIdentical) ul_skipIdentical= parseInt(localStorage.ul_skipIdentical);
 
 ulQueue.poke = function(file, meth)
 {

2958fd1b2c7bbaf9bda3dcadfe2d6a3c07f25001
Chris Riley <csr@mega.co.nz>
2015-02-23 11:00:20 +1300

    Added initialisation parameters for Credential Warning dialog

--- js/ui/credentialsWarningDialog.js
+++ js/ui/credentialsWarningDialog.js
@@ -39,17 +39,15 @@
                     'label': "Accept",
                     'className': "fm-dialog-button-green feedback-button-send",
                     'callback': function() {
-                        
-                        alert('accepted');
+                        CredentialsWarningDialog.acceptButtonCallback();
                         this.hide();
                     }
                 },
                 {
                     'label': "Reject",
                     'className': "fm-dialog-button-red feedback-button-cancel",
-                    'callback': function() {
-                        
-                        alert('rejected');
+                    'callback': function() {                        
+                        CredentialsWarningDialog.rejectButtonCallback();
                         this.hide();
                     }
                 }
@@ -72,46 +70,34 @@
     CredentialsWarningDialog.prototype._initGenericEvents = function() {
         var self = this;
         
-        // Test contact data (please replace)        
-        var contactEmail = 'email@domain.com';
-        var contactHandle = '4mmXu_081H4';
-        var seenOrVerifiedWording = 'verified'; // or seen
-                
-        // Test fingerprints (please replace)
-        var previousFingerprint = 'ABCDEF0123456789ABCDEF0123456789ABCDEF01';
-        var newFingerprint = 'ABCDFF0123456789ABCDEE0123456788ABCDEF00';
-        
         // Renders the dialog details, also shows the previous and new fingerprints differences in red
-        this._renderDetails(contactEmail, contactHandle, seenOrVerifiedWording);
-        this._renderFingerprints(previousFingerprint, newFingerprint);
+        this._renderDetails();
+        this._renderFingerprints();
         
         mega.ui.Dialog.prototype._initGenericEvents.apply(self);
     };
     
     /**
      * Render the placeholder details in the dialog
-     * @param {String} contactEmail The contact's email address
-     * @param {String} contactHandle The contact's user handle
-     * @param {String} seenOrVerifiedWording Can be 'seen' if the fingerprint has just been seen, or 'verified' if they previously verified the fingerprints
      */
-    CredentialsWarningDialog.prototype._renderDetails = function(contactEmail, contactHandle, seenOrVerifiedWording) {
-      
-        $dialog = $('.credentials-warning-dialog');        
-        $dialog.find('.email').text(contactEmail);
-        $dialog.find('.seenOrVerifiedWording').text(seenOrVerifiedWording);
+    CredentialsWarningDialog.prototype._renderDetails = function() {
         
-        if (typeof avatars[contactHandle] !== 'undefined') {
-            $dialog.find('.userAvatar img').attr('src', avatars[contactHandle].url);
+        $dialog = $('.credentials-warning-dialog');        
+        $dialog.find('.emailAddress').text(CredentialsWarningDialog.contactEmail);
+        $dialog.find('.seenOrVerified').text(CredentialsWarningDialog.seenOrVerifiedWording);
+        
+        if (typeof avatars[CredentialsWarningDialog.contactHandle] !== 'undefined') {
+            $dialog.find('.userAvatar img').attr('src', avatars[CredentialsWarningDialog.contactHandle].url);
         }
     };
     
     /**
      * Renders the previous and new fingerprints showing the differences in red
-     * @param {String} previousFingerprint The previously seen or verified fingerprint in hexadecimal
-     * @param {String} newFingerprint The new unverified fingerprint in hexadecimal
      */
-    CredentialsWarningDialog.prototype._renderFingerprints = function(previousFingerprint, newFingerprint) {
+    CredentialsWarningDialog.prototype._renderFingerprints = function() {
         
+        var previousFingerprint = CredentialsWarningDialog.previousFingerprint;
+        var newFingerprint = CredentialsWarningDialog.newFingerprint;
         var previousFingerprintHtml = '';
         var newFingerprintHtml = '';
         
@@ -158,12 +144,28 @@
 
     CredentialsWarningDialog.prototype = $.extend({}, mega.ui.Dialog.prototype, CredentialsWarningDialog.prototype);
 
-    CredentialsWarningDialog.singleton = function() {
+    /**
+     * Initialises the Credentials Warning Dialog
+     * @param {String} contactHandle The contact's user handle
+     * @param {Number} authMethod Whether the fingerprint is seen or verified, use authring.AUTHENTICATION_METHOD.SEEN or .FINGERPRINT_COMPARISON
+     * @param {String} prevFingerprint The previous fingerprint as a hexadecimal string
+     * @param {String} newFingerprint The current fingerprint as a hexadecimal string
+     * @param {Function} rejectBtnCallback What happens when the Reject fingerprint change button is clicked, e.g. disconnect the call/chat
+     * @param {Function} acceptBtnCallback What happens when the Accept fingerprint change button is clicked, e.g. proceed with the call/chat
+     * @returns {CredentialsWarningDialog._instance}
+     */
+    CredentialsWarningDialog.singleton = function(contactHandle, authMethod, prevFingerprint, newFingerprint, rejectBtnCallback, acceptBtnCallback) {
         
-        if (!CredentialsWarningDialog._instance) {
-            CredentialsWarningDialog._instance = new CredentialsWarningDialog();
-        }
-
+        // Set to object so can be used later
+        CredentialsWarningDialog.contactHandle = contactHandle;
+        CredentialsWarningDialog.contactEmail = M.u[contactHandle].m;
+        CredentialsWarningDialog.seenOrVerifiedWording = (authMethod === authring.AUTHENTICATION_METHOD.SEEN) ? 'seen' : 'verified';
+        CredentialsWarningDialog.previousFingerprint = prevFingerprint;
+        CredentialsWarningDialog.newFingerprint = newFingerprint;
+        CredentialsWarningDialog.rejectButtonCallback = rejectBtnCallback;
+        CredentialsWarningDialog.acceptButtonCallback = acceptBtnCallback;
+        
+        CredentialsWarningDialog._instance = new CredentialsWarningDialog();
         CredentialsWarningDialog._instance.show();
 
         return CredentialsWarningDialog._instance;

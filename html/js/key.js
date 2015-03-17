050ff74ade9238e21052011c41a7bbc0dc8d3f0c
andreidymovich <maximator2@mail.ru>
2015-03-17 09:07:01 +0300

    Improtant Registration Step 3 fix for old payment system
    #key page to test

--- html/js/key.js
+++ html/js/key.js
@@ -109,8 +109,8 @@ function ui_keycomplete()
 {
 	$('.key1').addClass('hidden');
 	$('.key2').addClass('hidden');
+    $('.key3').removeClass('hidden');
 
-	document.location.hash = 'pro';
 
 	if(typeof(u_attr.p) !== 'undefined' && (u_attr.p == 1 || u_attr.p == 2 || u_attr.p == 3)) {
 		document.location.hash = 'fm';

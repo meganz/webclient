f409a62fc4ac5f70a6a2e5b38a1216848d415c28
César D. Rodas <crodas@php.net>
2015-04-24 19:17:48 -0400

    Fixed tabs to space

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,54 +2,54 @@ var fs = require('fs');
 
 module.exports = function(grunt) {
 
-	var secure = fs.readFileSync("secureboot.js").toString().split("\n")
+    var secure = fs.readFileSync("secureboot.js").toString().split("\n")
 
     /* move to a plugin {{{ */
-	var htmls = [], htmlExtra = [], js = {}, code = [], has = {}
-	secure.forEach(function(l) {
+    var htmls = [], htmlExtra = [], js = {}, code = [], has = {}
+    secure.forEach(function(l) {
         var include = true
-		if (l.match(/f:.+\.js.+g:/)) {
-			eval("var y = " + l.match(/{[^}]+}/)[0])
-			if (y.g && y.f) {
-				if (!js[y.g]) js[y.g] = []
+        if (l.match(/f:.+\.js.+g:/)) {
+            eval("var y = " + l.match(/{[^}]+}/)[0])
+            if (y.g && y.f) {
+                if (!js[y.g]) js[y.g] = []
                 else include = false
-				js[y.g].push(y.f)
-			}
+                js[y.g].push(y.f)
+            }
             l = "/*placeholder-" + y.g + "*/"
-		} else if (l.indexOf(".html") > 1) {
-			if (l.indexOf("jsl.push") > 1) {
-				htmls.push( "build/html/" + l.match(/\/(.+.html)/)[1] )	
+        } else if (l.indexOf(".html") > 1) {
+            if (l.indexOf("jsl.push") > 1) {
+                htmls.push( "build/html/" + l.match(/\/(.+.html)/)[1] )    
                 l = 'jsl.push({f: "html/boot.json", n:"prod_assets_boot", j:9})'
                 include = !has['html']
                 has['html'] = true
-			} else if (l.indexOf(":") > 1) {
-				htmlExtra.push( "build/html/" + l.match(/\/(.+.html)/)[1] )	
+            } else if (l.indexOf(":") > 1) {
+                htmlExtra.push( "build/html/" + l.match(/\/(.+.html)/)[1] )    
                 l = l.replace(/html\/[^\.]+\.html/, "html/extra.json")
-			}
-		}
+            }
+        }
         if (include) code.push(l)
-	});
+    });
     code = code.join("\n")
 
-	var concat = {}, uglify = {}
+    var concat = {}, uglify = {}
 
-	for (var i in js) {
-		concat[i] = {
-			src: js[i],
-			dest: "js/xmega-" + i + ".js"
-		}
-		uglify[i] = {
-			options: {
-				sourceMap: true,
-			},
-			src: "js/xmega-" + i + ".js",
-			dest: "js/xmega-" + i + ".js",
-		}
+    for (var i in js) {
+        concat[i] = {
+            src: js[i],
+            dest: "js/xmega-" + i + ".js"
+        }
+        uglify[i] = {
+            options: {
+                sourceMap: true,
+            },
+            src: "js/xmega-" + i + ".js",
+            dest: "js/xmega-" + i + ".js",
+        }
         code = code.replace(
             "/*placeholder-" + i+"*/", 
             "jsl.push({f:'js/xmega-"+i+ ".js', n: 'pack_"+i+"', j:1});"
         )
-	}
+    }
     fs.writeFileSync("secureboot.prod.js", code)
     /* }}} */
 
@@ -57,24 +57,24 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         uglify: uglify,
-		concat: concat, 
-		htmlmin: {
-			default_options: {
-				options: {
-					removeComments: true,
-					collapseWhitespace: true,
-				},
-				files:[
-					 {expand: true, src: ['html/*.html', 'html/**/*.html'], dest: 'build/'},
-				],
-			},
-		},
+        concat: concat, 
+        htmlmin: {
+            default_options: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                },
+                files:[
+                     {expand: true, src: ['html/*.html', 'html/**/*.html'], dest: 'build/'},
+                ],
+            },
+        },
         htmljson: {
-			required: {
+            required: {
                 src: htmls,
                 dest: "html/boot.json",
             },
-			extra: {
+            extra: {
                 src: htmlExtra,
                 dest: "html/extra.json",
             },
@@ -84,8 +84,8 @@ module.exports = function(grunt) {
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-htmljson');
-	grunt.loadNpmTasks('grunt-contrib-htmlmin');
-	grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
 
     // Default task(s).

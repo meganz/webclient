8f44acec28eb0c393ae23d6e236df286189caf23
César D. Rodas <crodas@php.net>
2015-04-29 21:10:43 -0400

    Preparing HTML for translations
    Adding `{{ }}` syntax sugar to replace strings

--- scripts/translation.php
+++ scripts/translation.php
@@ -3,14 +3,18 @@
 $eng = json_decode(file_get_contents(__DIR__ . "/../lang/en.json"), true);
 $leng = array_map('strtolower', $eng);
 $eng = array_merge(array_flip($eng), array_flip($leng));
+$html = array();
 $new = array();
 
 foreach (glob(__DIR__ . "/../html/*.html") as $file) {
     $html = file_get_contents($file);
-    $html = preg_replace_callback("/{{([^}]+)}}/smU", function($match) use ($eng, &$new, $file) {
+    $html = preg_replace_callback("/{{([^}]+)}}/smU", function($match) use ($eng, &$new, $file, &$html) {
         $text = preg_replace("/\W+/",  " ", $match[1]);
         $text = trim($text);
         $ltext = strtolower($text);
+        if ($text != strip_tags($text)) {
+            return $match[0];
+        }
         if (!empty($eng[$text])) {
             return '[$' . $eng[$text]  . ']';
         }
@@ -19,8 +23,6 @@ foreach (glob(__DIR__ . "/../html/*.html") as $file) {
         }
 
         $new[] = $text;
-        print_r($eng);
-        var_dump($ltext, $text, $file);exit;
 
         return $match[0];
     }, $html);
@@ -28,4 +30,5 @@ foreach (glob(__DIR__ . "/../html/*.html") as $file) {
     file_put_contents($file, $html);
 }
 
+file_put_contents(__DIR__ . "/html.json", json_encode($html));
 file_put_contents(__DIR__ . '/new.json', json_encode(array_values(array_unique($new)), JSON_PRETTY_PRINT));

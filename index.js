5e58b6da90b513aa155c65d26fa1f3b30ce31844
Cesar Rodas <cr@mega.co.nz>
2015-06-21 17:54:00 -0400

    Render top avatar properly

--- index.js
+++ index.js
@@ -1713,6 +1713,7 @@ function parsetopmenu() {
     if (document.location.href.substr(0, 19) == 'chrome-extension://') {
         top = top.replace(/\/#/g, '/' + urlrootfile + '#');
     }
+    top = top.replace("{avatar-top}", useravatar.top());
     top = translate(top);
     return top;
 }

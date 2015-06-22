bfd408a3d947e44c00b27f714126fa986fe5a2f0
diegocr <dc@mega.co.nz>
2015-06-23 01:11:21 +0200

    Removed Function.bind polyfill..

--- test/config.js
+++ test/config.js
@@ -1,28 +1,2 @@
 asmCrypto.random.skipSystemRNGWarning = true;
 localStorage.minLogLevel = 50;
-
-// PhantomJS doesn't have Function.bind atm
-if (!Function.prototype.bind) {
-  Function.prototype.bind = function(oThis) {
-    if (typeof this !== 'function') {
-      // closest thing possible to the ECMAScript 5
-      // internal IsCallable function
-      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
-    }
-
-    var aArgs   = Array.prototype.slice.call(arguments, 1),
-        fToBind = this,
-        fNOP    = function() {},
-        fBound  = function() {
-          return fToBind.apply(this instanceof fNOP
-                 ? this
-                 : oThis,
-                 aArgs.concat(Array.prototype.slice.call(arguments)));
-        };
-
-    fNOP.prototype = this.prototype;
-    fBound.prototype = new fNOP();
-
-    return fBound;
-  };
-}

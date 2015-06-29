18ec7835860d97b80f08a16d62669f0f18e0d688
Chris <csr@mega.co.nz>
2015-06-29 16:13:16 +1200

    Hide UnionPay until ready

--- html/js/pro.js
+++ html/js/pro.js
@@ -659,14 +659,14 @@ var proPage = {
             supportsRecurring: false,
             supportsMonthlyPayment: false,  // Accept for 1 year one-time payment only
             cssClass: 'wire-transfer'
-        },
+        }/*,
         {
             apiGatewayId: 5,
             displayName: l[7109],           // UnionPay
             supportsRecurring: false,
             supportsMonthlyPayment: true,
             cssClass: 'union-pay'
-        }];
+        }*/];
 
         var html = '';
 

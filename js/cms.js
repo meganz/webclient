215cb0ec2b8fa706aadd3f44f3c76c6e9d27b25e
Cesar Rodas <cr@mega.co.nz>
2015-07-08 18:58:53 -0400

    Improves to CMS rendering
    1. Improve coding style
    2. Improve loading animation, it doensn't fade until the content is available

--- js/cms.js
+++ js/cms.js
@@ -6,286 +6,293 @@ var pubkey = ab_to_str(asmCrypto.base64_to_bytes('gVbVNtVJf210qJLe+GxWX8w9mC+WPn
 
 function verify_cms_content(content, signature)
 {
-	var hash = asmCrypto.SHA256.hex(content)
-	signature = ab_to_str(signature)
+    var hash = asmCrypto.SHA256.hex(content);
+    signature = ab_to_str(signature);
 
-	try {
-		return jodid25519.eddsa.verify(signature, hash, pubkey);
-	} catch (e) {
-		/* rubbish data, invalid anyways */
-		return false;
-	}
+    try {
+        return jodid25519.eddsa.verify(signature, hash, pubkey);
+    } catch (e) {
+        /* rubbish data, invalid anyways */
+        return false;
+    }
 }
 
 function process_cms_response(bytes, next, as, id)
 {
-	var viewer = new Uint8Array(bytes)
+    var viewer = new Uint8Array(bytes);
 
-	var signature = bytes.slice(3, 67); // 64 bytes, signature
-	var version = viewer[0];
-	var mime = viewer[1];
-	var label = ab_to_str(bytes.slice(67, viewer[2]+67));
-	var content = bytes.slice(viewer[2]+67)
+    var signature = bytes.slice(3, 67); // 64 bytes, signature
+    var version = viewer[0];
+    var mime = viewer[1];
+    var label = ab_to_str(bytes.slice(67, viewer[2]+67));
+    var content = bytes.slice(viewer[2]+67);
 
-	delete bytes;
+    if (as === "download") {
+        mime = 0;
+    }
 
-	if (as == "download") mime = 0;
+    if (verify_cms_content(content, signature)) {
+        switch (mime) {
+        case 3: // html
+            content = ab_to_str(content);
+            next(false, { html: content, mime: mime});
+            return loaded(id);
+            break;
 
-	if (verify_cms_content(content, signature)) {
-		switch (mime) {
-		case 3: // html
-			content = ab_to_str(content)
-			 next(false, { html: content, mime: mime})
-			 return loaded(id);
-			break;
+        case 1:
+            var blob = new Blob([content]);
+            content = window.URL.createObjectURL(blob);
+            next(false, { url: content, mime: mime});
+            return loaded(id);
 
-		case 1:
-			var blob = new Blob([content]);
-			content = window.URL.createObjectURL(blob);
-			next(false, { url: content, mime: mime})
-			return loaded(id);
+        case 2:
+            try {
+                content = JSON.parse(ab_to_str(content));
+            } catch (e) {
+                /* invalid json, weird case */
+                return next(true, {signature: false});
+            }
+            next(false, { object: content, mime: mime});
+            return loaded(id);
 
-		case 2:
-			try {
-				content = JSON.parse(ab_to_str(content))
-			} catch (e) {
-				/* invalid json, weird case */
-				return next(true, {signature: false});
-			}
-			next(false, { object: content, mime: mime})
-			return loaded(id);
-
-		default:
-			var io = new MemoryIO("temp", {});
-			io.begin = function() {};
-			io.setCredentials("", content.byteLength, "", [], []);
-			io.write(content, 0, function() {
-				io.download(label, "");
-				next(false, {});
-				return loaded(id);
-			});
-			break;
-		}
-	} else {
-		next(true, {error: 'Invalid signature', signature: true} );
-	}
+        default:
+            var io = new MemoryIO("temp", {});
+            io.begin = function() {};
+            io.setCredentials("", content.byteLength, "", [], []);
+            io.write(content, 0, function() {
+                io.download(label, "");
+                next(false, {});
+                return loaded(id);
+            });
+            break;
+        }
+    } else {
+        next(true, {error: 'Invalid signature', signature: true} );
+    }
 }
 
-var assets = {}, cmsToId = null
+var assets = {};
+var cmsToId = null;
 var booting = false;
 
-var is_img
+var is_img;
 
 /**
- *	Rewrite links. Basically this links 
+ *    Rewrite links. Basically this links
  *  shouldn't trigger the `CMS.get` and force
  *  a download
  */
 function dl_placeholder(str, sep, rid, id) {
-	return "'javascript:void(0)' data-cms-dl='"+id+"'"
+    return "'javascript:void(0)' data-cms-dl='"+id+"'";
 }
 
 /**
- *	Images placeholder. Replace *all* the images 
- *	with a placeholder until the image is fully loaded from 
+ *  Images placeholder. Replace *all* the images
+ *  with a placeholder until the image is fully loaded from
  *  the BLOB server
  */
 function img_placeholder(str, sep, rid, id) {
-	is_img = true;
-	return "'" + IMAGE_PLACEHOLDER + "' data-img='loading_" +  id + "'" 
+    is_img = true;
+    return "'" + IMAGE_PLACEHOLDER + "' data-img='loading_" +  id + "'";
 }
 
 function cmsObjectToId(name)
 {
-	var q = getxhr();
-	if (d) console.error("CMS: loading " + name)
-	q.onload = function() {
-		if (name == '_all') {
-			cmsToId = JSON.parse(ab_to_str(q.response));
-		} else {
-			cmsToId[name] = ab_to_str(q.response).split(".")
-		}
-		q = null;
-		if (name != '_all') doRequest(name);
-	}
-	q.onerror = function() {
-		Later(function() {
-			cmsObjectToId(name);
-		})
-		q = null;
-	};
-	var srv = apipath.replace(/https?:\/\//, '').replace(/\//g, '')
-	q.open("GET", (localStorage.cms || "https://cms.mega.nz/") + srv + '/' + name);
-	q.responseType = 'arraybuffer';
-	q.send();
+    var q = getxhr();
+    q.onload = function() {
+        if (name === '_all') {
+            cmsToId = JSON.parse(ab_to_str(q.response));
+        } else {
+            cmsToId[name] = ab_to_str(q.response).split(".");
+        }
+        q = null;
+        if (name !== '_all') {
+            doRequest(name);
+        }
+    };
+    q.onerror = function() {
+        Later(function() {
+            cmsObjectToId(name);
+        });
+        q = null;
+    };
+    var srv = apipath.replace(/https?:\/\//, '').replace(/\//g, '');
+    q.open("GET", (localStorage.cms || "https://cms.mega.nz/") + srv + '/' + name);
+    q.responseType = 'arraybuffer';
+    q.send();
 }
 
 /**
- *	Internal function to communicate with the BLOB server.
- *	
- *	It makes sure that optimize requests (makes sure we never
- *	ask things twice). This is the right place to 
- *	cache (perhaps towards localStorage).
+ *    Internal function to communicate with the BLOB server.
+ *
+ *    It makes sure that optimize requests (makes sure we never
+ *    ask things twice). This is the right place to
+ *    cache (perhaps towards localStorage).
  */
 var fetching = {};
 function doRequest(id) {
-	if (!id) throw new Error
-	if (d) console.error("CMS fetch element", id)
-	if (cmsToId === null) {
-		if (!booting) {
-			booting = true;
-			cmsObjectToId('_all');
-		}
-		return Later(function() {
-			doRequest(id);
-		});
-	}
-	if (!cmsToId[id]) {
-		return cmsObjectToId(id)
-	}
-	_cms_request(cmsToId[id], function(blob) {
-		for (var i in fetching[id]) {
-			process_cms_response(blob, fetching[id][i][0], fetching[id][i][1], id);
-		}
-		delete fetching[id];
-	});
+    if (!id) {
+        throw new Error("Calling CMS.doRequest without an ID");
+    }
+    if (cmsToId === null) {
+        if (!booting) {
+            booting = true;
+            cmsObjectToId('_all');
+        }
+        return Later(function() {
+            doRequest(id);
+        });
+    }
+    if (!cmsToId[id]) {
+        return cmsObjectToId(id);
+    }
+    _cms_request(cmsToId[id], function(blob) {
+        for (var i in fetching[id]) {
+            process_cms_response(blob, fetching[id][i][0], fetching[id][i][1], id);
+        }
+        delete fetching[id];
+    });
 }
 
 var _listeners = {};
 
 function loaded(id)
 {
-	if (_listeners[id]) {
-		for (var i in _listeners[id]) {
-			_listeners[id][i]();
-		}
-	}
-	CMS.attachEvents();
+    if (_listeners[id]) {
+        for (var i in _listeners[id]) {
+            _listeners[id][i]();
+        }
+    }
+    CMS.attachEvents();
 }
 
 function _concat_arraybuf(arr)
 {
-	var len = arr.reduce(function(prev, e) {
-		return prev+e.byteLength
-	}, 0);
-	var buffer = new Uint8Array(len)
-	var offset = 0
-	for (var i in arr) {
-		buffer.set(new Uint8Array(arr[i]), offset)
-		offset += arr[i].byteLength
-	}
-	return buffer.buffer
+    var len = arr.reduce(function(prev, e) {
+        return prev+e.byteLength;
+    }, 0);
+    var buffer = new Uint8Array(len);
+    var offset = 0;
+    for (var i in arr) {
+        buffer.set(new Uint8Array(arr[i]), offset);
+        offset += arr[i].byteLength;
+    }
+    return buffer.buffer;
 }
 
 function _cms_request(ids, next)
 {
-	if (d) console.error("CMS: request", ids)
-	var args = []
-		, q  = []
-		, done = 0
-	for (var i in ids) {
-		args.push({fa:i+":1*" + ids[i], k:i, plaintext: true})
-		q[i] = null
-	}
+    var args = [];
+    var q  = [];
+    var done = 0;
+    for (var i in ids) {
+        args.push({fa:i+":1*" + ids[i], k:i, plaintext: true});
+        q[i] = null;
+    }
 
-	api_getfileattr(args, 1, function(ctx, id, bytes)
-	{
-		if (d) console.error("Got response", id, bytes.byteLength, ctx)
-		
-		q[id] = bytes
-		if (++done == q.length) {
-			next(_concat_arraybuf(q))
-			q = undefined
-		}
-	});
+    api_getfileattr(args, 1, function(ctx, id, bytes)
+    {
+        q[id] = bytes;
+        if (++done === q.length) {
+            next(_concat_arraybuf(q));
+        }
+    });
 }
 
 var curType;
 var curCallback;
 
 var CMS = {
-	watch: function(type, callback)
-	{
-		curType = type;
-		curCallback = callback;
-	},
+    watch: function(type, callback)
+    {
+        curType = type;
+        curCallback = callback;
+    },
 
-	reRender: function(type, nodeId)
-	{
-		//ERRDEBUG(type, nodeId)
-		// If cmsToId is NULL it means we didn't open
-		// *any* CMS content so we should ignore this
-		// update, we will get the newest version always
-		// when we need it (the first time)
-		if (!(cmsToId instanceof Object)) return;
+    reRender: function(type, nodeId)
+    {
+        //ERRDEBUG(type, nodeId)
+        // If cmsToId is NULL it means we didn't open
+        // *any* CMS content so we should ignore this
+        // update, we will get the newest version always
+        // when we need it (the first time)
+        if (!(cmsToId instanceof Object)) {
+            return;
+        }
 
-		cmsToId[type] = nodeId;
-		if (type == curType) {
-			curCallback(nodeId);
-		}
-	},
+        cmsToId[type] = nodeId;
+        if (type === curType) {
+            curCallback(nodeId);
+        }
+    },
 
-	attachEvents: function() {
-		$('*[data-cms-dl],.cms-asset-download').rebind('click', function(e) {
-			var $this = $(this)
-				, target = $this.data('id') || $this.data('cms-dl');
-			if (!target) return;
+    isLoading: function() {
+        return Object.keys(fetching).length > 0;
+    },
 
-			e.preventDefault();
+    attachEvents: function() {
+        $('*[data-cms-dl],.cms-asset-download').rebind('click', function(e) {
+            var $this  = $(this);
+            var target = $this.data('id') || $this.data('cms-dl');
+            if (!target) {
+                return;
+            }
 
-			loadingDialog.show();
-			CMS.get(target, function() {
-				loadingDialog.hide();
-			}, 'download');
+            e.preventDefault();
 
-			return false;
-		});
-	},
+            loadingDialog.show();
+            CMS.get(target, function() {
+                loadingDialog.hide();
+            }, 'download');
 
-	loaded: loaded,
+            return false;
+        });
+    },
 
-	img : function(id) {
-		if (!assets[id]) {
-			this.get(id, function(err, obj) {
-				$('*[data-img=loading_' + id + ']').attr({'id': '', 'src': obj.url})
-				assets[id] = obj.url;
-			});
-		}
-		return assets[id] ? assets[id] : IMAGE_PLACEHOLDER;
-	},
-	get: function(id, next, as) {
-		if (typeof fetching[id] == "undefined") {
-			doRequest(id);
-			fetching[id] = [];
-		}
-		next = next || function() {};
-		fetching[id].push([next, as]);
-	},
+    loaded: loaded,
 
-	on: function(id, callback)
-	{
-		if (!_listeners[id]) {
-			_listeners[id] = [];
-		}
-		_listeners[id].push(callback);
-	},
+    img : function(id) {
+        if (!assets[id]) {
+            this.get(id, function(err, obj) {
+                $('*[data-img=loading_' + id + ']').attr({'id': '', 'src': obj.url});
+                assets[id] = obj.url;
+            });
+        }
+        return assets[id] ? assets[id] : IMAGE_PLACEHOLDER;
+    },
+    get: function(id, next, as) {
+        if (typeof fetching[id] === "undefined") {
+            doRequest(id);
+            fetching[id] = [];
+        }
+        next = next || function() {};
+        fetching[id].push([next, as]);
+    },
 
-	imgLoader: function(html, id) {
-		if (!assets[id]) {
-			is_img = false;
-			// replace images
-			html = html.replace(new RegExp('([\'"])(i:(' + id + '))([\'"])', 'g'), img_placeholder);
-			// replace download links
-			html = html.replace(new RegExp('([\'"])(d:(' + id + '))([\'"])', 'g'), dl_placeholder);
-		
-			if (is_img) {
-				this.get(id);
-			}
-		} else {
-			html = html.replace(IMAGE_PLACEHOLDER + "' data-img='loading_" + id, assets[id], 'g');
-		}
-		return html;
-	}
+    on: function(id, callback)
+    {
+        if (!_listeners[id]) {
+            _listeners[id] = [];
+        }
+        _listeners[id].push(callback);
+    },
+
+    imgLoader: function(html, id) {
+        if (!assets[id]) {
+            is_img = false;
+            // replace images
+            html = html.replace(new RegExp('([\'"])(i:(' + id + '))([\'"])', 'g'), img_placeholder);
+            // replace download links
+            html = html.replace(new RegExp('([\'"])(d:(' + id + '))([\'"])', 'g'), dl_placeholder);
+        
+            if (is_img) {
+                this.get(id);
+            }
+        } else {
+            html = html.replace(IMAGE_PLACEHOLDER + "' data-img='loading_" + id, assets[id], 'g');
+        }
+        return html;
+    }
 };
 
 /* Make it public */
@@ -295,17 +302,17 @@ window.CMS = CMS;
 
 CMS.on('corporate', function()
 {
-	$('.new-left-menu-link').rebind('click', function() {
-		document.location.hash = "#corporate/" + $(this).attr('id')
-	});
-	var ctype = document.location.hash.substr(11)
-	if ($('#' + ctype).length == 1) {
-		$('.new-right-content-block').addClass('hidden');
-		$('.new-right-content-block.' + ctype).removeClass('hidden');
-		$('.new-left-menu-link').removeClass('active');
-		$('#' + ctype).addClass('active');			
-		mainScroll();
-	} else {
-		$('.new-left-menu-link:first').trigger('click');
-	}
+    $('.new-left-menu-link').rebind('click', function() {
+        document.location.hash = "#corporate/" + $(this).attr('id');
+    });
+    var ctype = document.location.hash.substr(11);
+    if ($('#' + ctype).length === 1) {
+        $('.new-right-content-block').addClass('hidden');
+        $('.new-right-content-block.' + ctype).removeClass('hidden');
+        $('.new-left-menu-link').removeClass('active');
+        $('#' + ctype).addClass('active');            
+        mainScroll();
+    } else {
+        $('.new-left-menu-link:first').trigger('click');
+    }
 });

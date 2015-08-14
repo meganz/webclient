e1a563294535a5cdf256394d51dadfaf9ef58909
Andrei Dymovich <ad@mega.co.nz>
2015-08-14 15:02:37 +0300

    Header Disappearing fix if user goes from FM on Help center page first time.

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ function init_page() {
         document.location.hash = 'signup' + localStorage.signupcode;
         return false;
     }
-    if (!page.match(/^(blog|corporate|page_)/)) {
+    if (!page.match(/^(blog|help|corporate|page_)/)) {
         $('.top-head').remove();
     }
     $('#loading').hide();

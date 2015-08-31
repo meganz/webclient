dc59ed924fd38a48631b13aaedc0b3d3c7edf01a
C R <csr@Cs-MacBook-Pro.local>
2015-08-31 18:37:31 +1200

    Try fix #newsignup links in Firefox extension

--- js/functions.js
+++ js/functions.js
@@ -358,18 +358,15 @@ function divscroll(el) {
 
 function removeHash() {
     var scrollV, scrollH, loc = window.location;
-    if ("pushState" in history) {
-        history.pushState("", document.title, loc.pathname + loc.search);
-    }
-    else {
-        // Prevent scrolling by storing the page's current scroll offset
-        scrollV = document.body.scrollTop;
-        scrollH = document.body.scrollLeft;
-        loc.hash = "";
-        // Restore the scroll offset, should be flicker free
-        document.body.scrollTop = scrollV;
-        document.body.scrollLeft = scrollH;
-    }
+    
+    // Prevent scrolling by storing the page's current scroll offset
+    scrollV = document.body.scrollTop;
+    scrollH = document.body.scrollLeft;
+    loc.hash = "";
+
+    // Restore the scroll offset, should be flicker free
+    document.body.scrollTop = scrollV;
+    document.body.scrollLeft = scrollH;
 }
 
 function browserdetails(useragent) {

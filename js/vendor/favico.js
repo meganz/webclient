b7683fe3014b893e162e13fb43bb8ab89761a305
Chris <csr@mega.co.nz>
2015-09-11 13:57:30 +1200

    Remove exotic character breaking the build script

--- js/vendor/favico.js
+++ js/vendor/favico.js
@@ -448,7 +448,7 @@
 			if (video.paused || video.ended || _stop) {
 				return false;
 			}
-			//nasty hack for FF webcam (Thanks to Julian Ćwirko, kontakt@redsunmedia.pl)
+			//nasty hack for FF webcam (Thanks to Julian Cwirko, kontakt@redsunmedia.pl)
 			try {
 				_context.clearRect(0, 0, _w, _h);
 				_context.drawImage(video, 0, 0, _w, _h);

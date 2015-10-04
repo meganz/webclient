86403194423be5527f8c066f85d1ec40bd90ec8e
Aleksandar Loncar <al@mega.co.nz>
2015-10-04 10:43:25 +0200

    Removed padding between public handle and decryption key in export/public link dialog. Strings replaced with dictionary keys.

--- js/fm.js
+++ js/fm.js
@@ -8519,8 +8519,8 @@ function itemExportLinkHtml(item) {
          +               '<span class="export-item-title">' + htmlentities(item.name) + '</span><span class="export-link-gray-txt">' + fileSize + '</span>'
          +          '</div>'
          +          '<div class="file-link-block">'
-         +              '<span class="file-link-info url" data-pseudo-content="public handle:">' + fileUrlWithoutKey + '</span>'
-         +          	'<span class="file-link-info key" data-pseudo-content="decryption key:">' + fileUrlKey + '</span>'
+         +              '<span class="file-link-info url" data-pseudo-content=' + l[2702] + '":">' + fileUrlWithoutKey + '</span>'
+         +          	'<span class="file-link-info key" data-pseudo-content="' + l[1028] + ':">' + fileUrlKey + '</span>'
          +          '</div>'
          +      '</div>'
          +  '</div>';

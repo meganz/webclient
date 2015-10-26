7d41001b63092dc38d2f656c8b84cd78592b2e15
Diego Casorran <dc@mega.co.nz>
2015-10-26 16:26:23 +0100

    New Karma launchers, tweaks/improvements, and unit tests.

--- test/config.js
+++ test/config.js
@@ -1,2 +1,23 @@
 asmCrypto.random.skipSystemRNGWarning = true;
-localStorage.minLogLevel = 50;
+
+function _showDebug(sandbox, stubs) {
+    window.d = 1;
+    MegaLogger.rootLogger.options.isEnabled = 1;
+
+    if (sandbox) {
+        stubs.forEach(function(fc) {
+            fc = fc.split('.');
+            if (fc.length < 2) {
+                sandbox.stub(window, fc[0]);
+            }
+            else {
+                sandbox.stub(window[fc[0]], fc[1]);
+            }
+        });
+    }
+}
+
+function _hideDebug() {
+    window.d = 0;
+    MegaLogger.rootLogger.options.isEnabled = 0;
+}

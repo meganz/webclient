a9058c51b443e7b62ac237d76822ebef5c716391
Diego Casorran <dc@mega.co.nz>
2015-10-29 00:36:47 +0100

    Removed buggy and unused popunda.js file.

--- secureboot.js
+++ secureboot.js
@@ -1307,7 +1307,6 @@ else if (!b_u)
     jsl.push({f:'html/fm.html', n: 'fm', j:0,w:3});
     jsl.push({f:'html/top.html', n: 'top', j:0});
     jsl.push({f:'js/notify.js', n: 'notify_js', j:1});
-    jsl.push({f:'js/popunda.js', n: 'popunda_js', j:1});
     jsl.push({f:'css/style.css', n: 'style_css', j:2,w:30,c:1,d:1,cache:1});
     jsl.push({f:'js/useravatar.js', n: 'contact_avatar_js', j:1,w:3});
     jsl.push({f:'js/vendor/avatar.js', n: 'avatar_js', j:1, w:3});

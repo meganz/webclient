8148c96884784cf920204b6614db80352af0bbce
Chris <csr@mega.co.nz>
2015-11-04 16:57:56 +1300

    Add remaining functionality for direct voucher redeem dialog

--- html/js/pro.js
+++ html/js/pro.js
@@ -1114,6 +1114,7 @@ var voucherRedeemDialog = {
     
     $dialog: null,
     $backgroundOverlay: null,
+    $successOverlay: null,
     
     voucherCode: '',
     accountBalance: 0,
@@ -1128,6 +1129,7 @@ var voucherRedeemDialog = {
         // Cache DOM reference for lookup in other functions
         this.$dialog = $('.voucher-redeem-dialog');
         this.$backgroundOverlay = $('.fm-dialog-overlay');
+        this.$successOverlay = $('.payment-result.success');
         
         // Init functions
         this.addVoucher();
@@ -1248,8 +1250,9 @@ var voucherRedeemDialog = {
         // Sort plans by lowest price first
         voucherRedeemDialog.membershipPlans.sort(function(planA, planB) {
 
-            var pricePlanA = planA[5];
-            var pricePlanB = planB[5];
+            // Convert from string for proper comparison
+            var pricePlanA = parseFloat(planA[5]);
+            var pricePlanB = parseFloat(planB[5]);
 
             if (pricePlanA < pricePlanB) {
                 return -1;
@@ -1261,15 +1264,23 @@ var voucherRedeemDialog = {
             return 0;
         });
         
+        var selectedPlanIndex = 0;
+        
         // Find the most expensive plan that they can purchase with their current account balance
-        for (var i = 0, selectedPlanIndex = 0; i < voucherRedeemDialog.membershipPlans.length; i++) {
+        for (var i = 0; i < voucherRedeemDialog.membershipPlans.length; i++) {
             
+            // Convert string price to float for correct comparison
             var planPrice = voucherRedeemDialog.membershipPlans[i][5];
+                planPrice = parseFloat(planPrice);
             
             // If their account balance is equal or more than the plan price, update
-            if (voucherRedeemDialog.accountBalance >= planPrice) {
+            if (planPrice <= voucherRedeemDialog.accountBalance) {
                 selectedPlanIndex = i;
-            }        
+            }
+            else {
+                // Exit out, the other plans are too expensive
+                break;
+            }
         }
         
         // Set the best plan for the user
@@ -1285,16 +1296,174 @@ var voucherRedeemDialog = {
     displayDialog: function() {
         
         var balanceString = voucherRedeemDialog.accountBalance.toFixed(2);
-        var planPrice = voucherRedeemDialog.bestPlan[5];
+        var planId = voucherRedeemDialog.bestPlan[0];
+        var proNum = voucherRedeemDialog.bestPlan[1];
+        var storage = voucherRedeemDialog.bestPlan[2];
+        var bandwidth = voucherRedeemDialog.bestPlan[3];
+        var numOfMonths = voucherRedeemDialog.bestPlan[4];
+        var planPrice = voucherRedeemDialog.bestPlan[5].split('.');
+        var proName = getProPlan(proNum);
         
-        console.info('best plan price' + planPrice, voucherRedeemDialog.bestPlan);
-                    
+        // Get dollars and cents
+        var planPriceDollars = planPrice[0];
+        var planPriceCents = planPrice[1];
+        
+        // Get 'month' or 'year' text
+        var monthOrYearText = (numOfMonths < 12) ? l[931] : l[932];
+        
+        // Convert larger storage sizes over 1024 GB to TB wording
+        var storageAmount = (storage < 1024) ? storage : (storage / 1024);        
+        var bandwidthAmount = (bandwidth < 1024) ? bandwidth : (bandwidth / 1024);
+        var storageUnit = (storage < 1024) ? 'GB' : 'TB';
+        var bandwidthUnit = (bandwidth < 1024) ? 'GB' : 'TB';
+                            
+        // Update information
+        voucherRedeemDialog.$dialog.find('.reg-st3-membership-bl').removeClass('pro1 pro2 pro3 pro4');
+        voucherRedeemDialog.$dialog.find('.reg-st3-membership-bl').addClass('pro' + proNum);
+        voucherRedeemDialog.$dialog.find('.title').html(proName);
+        voucherRedeemDialog.$dialog.find('.price .dollars').text(planPriceDollars);
+        voucherRedeemDialog.$dialog.find('.price .cents').text('.' + planPriceCents);
+        voucherRedeemDialog.$dialog.find('.price .period').text('/' + monthOrYearText);
+        voucherRedeemDialog.$dialog.find('.reg-st3-storage .quota-amount').text(storageAmount);
+        voucherRedeemDialog.$dialog.find('.reg-st3-storage .quota-unit').text(storageUnit);
+        voucherRedeemDialog.$dialog.find('.reg-st3-bandwidth .quota-amount').text(bandwidthAmount);
+        voucherRedeemDialog.$dialog.find('.reg-st3-bandwidth .quota-unit').text(bandwidthUnit);
         voucherRedeemDialog.$dialog.find('.balance').text(balanceString);
-        voucherRedeemDialog.$dialog.find('.voucher-amount').text(voucherRedeemDialog.voucherAmount);        
+        voucherRedeemDialog.$dialog.find('.voucher-amount').text(voucherRedeemDialog.voucherAmount);
+        voucherRedeemDialog.$dialog.find('.pro-plan').text(proName);
+        voucherRedeemDialog.$dialog.find('.complete-upgrade-button').attr('data-plan-id', planId);
         
+        // Show the dialog
         voucherRedeemDialog.showBackgroundOverlay();
         voucherRedeemDialog.$dialog.removeClass('hidden');
-    },    
+        
+        // Button functionality
+        voucherRedeemDialog.initCloseButton();
+        voucherRedeemDialog.initUpgradeButton();
+    },
+    
+    /**
+     * Closes the dialog and goes to the pro page
+     */
+    initCloseButton: function() {
+        
+        voucherRedeemDialog.$dialog.find('.payment-close-icon, .choose-plan-button').rebind('click', function() {
+            
+            // Hide the dialog
+            voucherRedeemDialog.hideBackgroundOverlay();
+            voucherRedeemDialog.$dialog.addClass('hidden');
+            
+            // Go to the #pro page
+            document.location.hash = 'pro';
+        });
+        
+        // Prevent close of dialog from clicking outside the dialog
+        $('.fm-dialog-overlay.payment-dialog-overlay').rebind('click', function(event) {
+            event.stopPropagation();
+        });
+    },
+    
+    /**
+     * Upgrades their account to the relevant plan
+     */
+    initUpgradeButton: function() {
+        
+        voucherRedeemDialog.$dialog.find('.complete-upgrade-button').rebind('click', function() {
+            
+            voucherRedeemDialog.$dialog.addClass('hidden');
+            voucherRedeemDialog.processProPurchaseWithBalance();            
+        });        
+    },
+    
+    /**
+     * Complete the Pro purchase using their balance and the relevant plan
+     */
+    processProPurchaseWithBalance: function() {
+        
+        // Data for API request
+        var apiId = voucherRedeemDialog.bestPlan[0];
+        var price = voucherRedeemDialog.bestPlan[5];
+        var currency = voucherRedeemDialog.bestPlan[6];
+        var gatewayId = 0;                                  // Prepay / account balance
+        
+        // Start loading spinner
+        loadingDialog.show();
+        
+        // User Transaction Sale API call
+        api_req({ a: 'uts', it: 0, si: apiId, p: price, c: currency }, {   
+            callback: function (utsResult) {
+
+                // Store the sale ID to check with API later
+                var saleId = utsResult;
+
+                // If an error
+                if (typeof saleId == 'number' && saleId < 0) {
+                    
+                    loadingDialog.hide();
+                    alert(l[200]);
+                    
+                    return false;
+                }
+                
+                // User Transaction Complete API call
+                api_req({ a: 'utc', s: [saleId], m: gatewayId }, {
+                    callback: function(utcResult) {
+
+                        // Hide the loading dialog
+                        loadingDialog.hide();
+
+                        // If an error code
+                        if (typeof utcResult == 'number' && utcResult < 0) {
+
+                            // Insufficient balance, please try again
+                            if (utcResult == EOVERQUOTA) {
+                                alert(l[514]);
+                            }
+                            else {
+                                // Oops, something went wrong
+                                alert(l[200]);
+                            }
+                        }
+                        else {
+                            // Show success dialog
+                            voucherRedeemDialog.showSuccessfulPayment();
+                        }
+                    }
+                });
+            }
+        });        
+    },
+    
+    /**
+     * Shows a successful payment modal dialog
+     */
+    showSuccessfulPayment: function() {
+        
+        // Get the selected Pro plan details
+        var proNum = voucherRedeemDialog.bestPlan[1];
+        var proPlan = getProPlan(proNum);
+        var successMessage = l[6962].replace('%1', '<span>' + proPlan + '</span>');
+        
+        // Show the success
+        voucherRedeemDialog.showBackgroundOverlay();
+        voucherRedeemDialog.$successOverlay.removeClass('hidden');
+        voucherRedeemDialog.$successOverlay.find('.payment-result-txt').html(successMessage);
+        
+        // Add click handlers for 'Go to my account' and Close buttons
+        voucherRedeemDialog.$successOverlay.find('.payment-result-button, .payment-close').rebind('click', function() {
+            
+            // Hide the overlay
+            voucherRedeemDialog.hideBackgroundOverlay();
+            voucherRedeemDialog.$successOverlay.addClass('hidden');
+            
+            // Make sure it fetches new account data on reload
+            // and redirect to account page to show purchase
+            if (M.account) {
+                M.account.lastupdate = 0;
+            }
+            window.location.hash = 'fm/account/history';
+        });
+    },
     
     /**
      * Shows the background overlay

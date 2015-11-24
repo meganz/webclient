6bdc25b1bfd33ea564ff4797ff67c31ac734d762
Chris <csr@mega.co.nz>
2015-11-24 16:16:21 +1300

    Translations for the extension pages

--- html/js/chrome.js
+++ html/js/chrome.js
@@ -74,18 +74,18 @@ var chromepage = {
     compareLocalToServerBuildVersion: function(serverBuildVersion) {
         
         // Default message shown if the Chrome extension is not installed
-        var message = 'Download the extension (v' + serverBuildVersion.chrome + ') below.';
+        var message = l[7873].replace('%1', serverBuildVersion.chrome);
                 
         // If current build information is available (not in development) and currently using the Chrome extension
         if ((buildVersion.chrome !== '') && (is_extension) && (ua.indexOf('chrome') > -1)) {
             
             // If the currently loaded version is older than the server build
             if (versionCompare(buildVersion.chrome, serverBuildVersion.chrome) === -1) {
-                message = 'Looks like your current extension (v' + buildVersion.chrome + ') is out of date. Download the latest (v' + serverBuildVersion.chrome + ') below.';
+                message = l[7874].replace('%1', buildVersion.chrome).replace('%2', serverBuildVersion.chrome);
             }
             else {
-                // Congrats, you're using the current version
-                message = 'Your extension (v' + serverBuildVersion.chrome + ') is up to date.';
+                // They're using the current version
+                message = l[7875].replace('%1', serverBuildVersion.chrome);
             }
         }
         
--- html/js/firefox.js
+++ html/js/firefox.js
@@ -50,18 +50,18 @@ var firefoxpage = {
     compareLocalToServerBuildVersion: function(serverBuildVersion) {
         
         // Default message shown if the Firefox extension is not installed
-        var message = 'Download the extension (v' + serverBuildVersion.firefox + ') below.';
+        var message = l[7873].replace('%1', serverBuildVersion.firefox);
                 
         // If current build information is available (not in development) and currently using the Firefox extension
         if ((buildVersion.firefox !== '') && (is_extension) && (ua.indexOf('firefox') > -1)) {
             
             // If the currently loaded version is older than the server build
             if (versionCompare(buildVersion.firefox, serverBuildVersion.firefox) === -1) {
-                message = 'Looks like your current extension (v' + buildVersion.firefox + ') is out of date. Download the latest (v' + serverBuildVersion.firefox + ') below.';
+                message = l[7874].replace('%1', buildVersion.firefox).replace('%2', serverBuildVersion.firefox);;
             }
             else {
-                // Congrats, you're using the current version
-                message = 'Your extension (v' + serverBuildVersion.firefox + ') is up to date.';
+                // They're using the current version
+                message = l[7875].replace('%1', serverBuildVersion.firefox);
             }
         }
         

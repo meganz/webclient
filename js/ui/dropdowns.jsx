6cb25b7aa433b55837b225dadeb3af1e2719df02
Lyubomir Petrov <lp@mega.co.nz>
2015-12-15 19:31:09 +0200

    major emoji performance optimisation;

--- js/ui/dropdowns.jsx
+++ js/ui/dropdowns.jsx
@@ -237,7 +237,12 @@ var DropdownEmojiSelector = React.createClass({
         return {
             'previewEmoji': null,
             'searchValue': '',
-            'browsingCategory': false
+            'browsingCategory': false,
+            'isActive': false,
+            'visibleEmojis': [].concat(
+                Object.keys(EMOJILIST.EMOJI_CATEGORIES["FREQUENTLY USED"]),
+                Object.keys(EMOJILIST.EMOJI_CATEGORIES["PEOPLE"])
+            )
         }
     },
     onSearchChange: function(e) {
@@ -248,174 +253,256 @@ var DropdownEmojiSelector = React.createClass({
         });
         $('.popup-scroll-area.emoji-one:visible').data('jsp').scrollTo(0);
     },
+    componentDidMount: function() {
+        $(window).rebind(
+            //'DOMContentLoaded.lazyRenderer' + this.getUniqueId() + ' ' +
+            //'load.lazyRenderer' + this.getUniqueId() + ' ' +
+            'resize.lazyRenderer' + this.getUniqueId() + ' ' +
+            //'hashchange.lazyRenderer' + this.getUniqueId() + ' ' +
+            'scroll.lazyRenderer' + this.getUniqueId(),
+            this.requiresLazyRendering
+        );
+
+        this.requiresLazyRendering();
+    },
+    componentWillUnmount: function() {
+        //$(window).unbind('DOMContentLoaded.lazyRenderer' + this.getUniqueId());
+        //$(window).unbind('load.lazyRenderer' + this.getUniqueId());
+        $(window).unbind('resize.lazyRenderer' + this.getUniqueId());
+        //$(window).unbind('hashchange.lazyRenderer' + this.getUniqueId());
+        $(window).unbind('scroll.lazyRenderer' + this.getUniqueId());
+    },
+    _getVisibleEmojis: function() {
+        var $container = $('.popup.emoji-one:visible');
+
+        var $emojis = $('.button.square-button.emoji-one[data-emoji]', $container);
+
+        if ($emojis.size() === 0) {
+            return false;
+        }
+
+        var inViewport = [];
+        $emojis.each(function() {
+            if (elementInViewport2(this)) {
+                inViewport.push($(this).attr('data-emoji'));
+            }
+        });
+
+        return inViewport;
+    },
+    requiresLazyRendering: function(e) {
+        if (!this.isMounted()) {
+            return;
+        }
+
+        var $container = $(this.findDOMNode());
+        if (!$container.is(":visible")) {
+            return;
+        }
+
+        var inViewport = this._getVisibleEmojis();
+
+        if (inViewport !== false) {
+            this.setState({'visibleEmojis': inViewport});
+        }
+    },
     render: function() {
         var self = this;
 
-        var preview;
-        if (self.state.previewEmoji) {
-            var slug = self.state.previewEmoji;
-            var emojiMeta = EMOJILIST.EMOJIS[slug];
-            var txt = ":" + slug + ":";
-            if (slug.substr(0, 1) == ":" || slug.substr(-1) == ":") {
-                txt = slug;
-            }
+        var popupContents = null;
 
-            preview = <div className="emoji-one-preview">
-                <span className={"emoji-one demo-icon emojione-" + emojiMeta[0]}></span>
-                <div className="emoji-one title">{txt}</div>
-            </div>;
-        }
-
-
-        var emojis = [];
-        Object.keys(EMOJILIST.EMOJI_CATEGORIES).forEach(function(categoryName) {
-            if (
-                self.state.browsingCategory !== "FREQUENTLY USED" &&
-                self.state.browsingCategory !== false &&
-                self.state.browsingCategory != categoryName
-            ) {
-                return;
-            }
-
-            var curCategoryEmojis = [];
-            Object.keys(EMOJILIST.EMOJI_CATEGORIES[categoryName]).forEach(function(slug) {
-                if (self.state.searchValue.length > 0) {
-                    if (slug.indexOf(self.state.searchValue) < 0) {
-                        return;
-                    }
+        if (self.state.isActive === true) {
+            var preview;
+            if (self.state.previewEmoji) {
+                var slug = self.state.previewEmoji;
+                var emojiMeta = EMOJILIST.EMOJIS[slug];
+                var txt = ":" + slug + ":";
+                if (slug.substr(0, 1) == ":" || slug.substr(-1) == ":") {
+                    txt = slug;
                 }
-                var meta = EMOJILIST.EMOJIS[slug];
 
-                curCategoryEmojis.push(
+
+                preview = <div className="emoji-one-preview">
+                    <span className={"emoji-one demo-icon emojione-" + emojiMeta[0]}></span>
+                    <div className="emoji-one title">{txt}</div>
+                </div>;
+            }
+
+
+            var emojis = [];
+            var visibleEmojis = this._getVisibleEmojis();
+            if (visibleEmojis === false) {
+                visibleEmojis = self.state.visibleEmojis;
+            }
+            Object.keys(EMOJILIST.EMOJI_CATEGORIES).forEach(function (categoryName) {
+                if (
+                    self.state.browsingCategory !== "FREQUENTLY USED" &&
+                    self.state.browsingCategory !== false &&
+                    self.state.browsingCategory != categoryName
+                ) {
+                    return;
+                }
+
+                var curCategoryEmojis = [];
+                Object.keys(EMOJILIST.EMOJI_CATEGORIES[categoryName]).forEach(function (slug) {
+                    if (self.state.searchValue.length > 0) {
+                        if (slug.indexOf(self.state.searchValue) < 0) {
+                            return;
+                        }
+                    }
+                    var meta = EMOJILIST.EMOJIS[slug];
+
+                    if (visibleEmojis.indexOf(slug) > -1) {
+                        curCategoryEmojis.push(
+                            <div
+                                data-emoji={slug}
+                                className="button square-button emoji-one" key={categoryName + "_" + slug}
+                                onMouseEnter={(e) => {
+                                if (self.mouseEnterTimer) {
+                                    clearTimeout(self.mouseEnterTimer);
+                                }
+
+                                e.stopPropagation();
+                                e.preventDefault();
+
+                                // delay the .setState change, because of the tons of icons we've, which are re-rendered
+                                // in case of .setState
+                                self.mouseEnterTimer = setTimeout(function() {
+                                    self.setState({'previewEmoji': slug});
+                                }, 75);
+                            }}
+                                onMouseLeave={(e) => {
+                                if (self.mouseEnterTimer) {
+                                    clearTimeout(self.mouseEnterTimer);
+                                }
+                                e.stopPropagation();
+                                e.preventDefault();
+
+                                self.setState({'previewEmoji': null});
+                            }}
+                                onClick={(e) => {
+                                if (self.props.onClick) {
+                                    self.props.onClick(e, slug, meta);
+                                }
+                            }}>
+                            <span
+                                className={"emojione-" + meta[0]}
+                                title={":" + slug + ":"}
+                                dangerouslySetInnerHTML={{__html: meta[1]}}></span>
+                            </div>
+                        );
+                    }
+                    else {
+                        curCategoryEmojis.push(
+                            <div
+                                data-emoji={slug}
+                                className="button square-button emoji-one placeholder"
+                                key={categoryName + "_" + slug + "_pl"}
+                            />
+                        );
+                    }
+                });
+
+                if (curCategoryEmojis.length > 0) {
+                    emojis.push(
+                        <div key={categoryName}>
+                            {emojis.length > 0 ? <div className="clear"></div> : null}
+                            <div className="emoji-type-txt">
+                                {__(categoryName)}
+                            </div>
+
+                            <div className="clear"></div>
+                            {curCategoryEmojis}
+                            <div className="clear"></div>
+                        </div>
+                    );
+                }
+            });
+            var categoryIcons = {
+                "FREQUENTLY USED": "clock-icon",
+                "PEOPLE": "smile-icon",
+                "NATURE": "sun-icon",
+                "FOOD & DRINK": "wineglass-icon",
+                "CELEBRATION": "present-icon",
+                "ACTIVITY": "bowling-ball-icon",
+                "TRAVEL & PLACES": "earth-icon",
+                "OBJECTS & SYMBOLS": "percents-icon"
+            };
+
+            var categoryButtons = [];
+
+            Object.keys(categoryIcons).forEach(function (categoryName) {
+                var activeClass = self.state.browsingCategory === categoryName ? " active" : "";
+
+                if (
+                    self.state.browsingCategory === false &&
+                    self.state.searchValue === '' &&
+                    categoryIcons[categoryName] === "clock-icon"
+                ) {
+                    activeClass = " active";
+                }
+
+                categoryButtons.push(
                     <div
-                        className="button square-button emoji-one" key={categoryName + "_" + slug}
-                        onMouseEnter={(e) => {
-                            if (self.mouseEnterTimer) {
-                                clearTimeout(self.mouseEnterTimer);
-                            }
-
-                            e.stopPropagation();
-                            e.preventDefault();
-
-                            // delay the .setState change, because of the tons of icons we've, which are re-rendered
-                            // in case of .setState
-                            self.mouseEnterTimer = setTimeout(function() {
-                                self.setState({'previewEmoji': slug});
-                            }, 75);
-                        }}
-                        onMouseLeave={(e) => {
-                            if (self.mouseEnterTimer) {
-                                clearTimeout(self.mouseEnterTimer);
-                            }
-                            e.stopPropagation();
-                            e.preventDefault();
-
-                            self.setState({'previewEmoji': null});
-                        }}
+                        className={"button square-button emoji-one" + (activeClass)}
+                        key={categoryIcons[categoryName]}
                         onClick={(e) => {
-                            if (self.props.onClick) {
-                                self.props.onClick(e, slug, meta);
-                            }
-                        }}>
-                        <span
-                            className={"emojione-" + meta[0]}
-                            title={":" + slug + ":"}
-                            dangerouslySetInnerHTML={{__html: meta[1]}}></span>
+                            e.stopPropagation();
+                            e.preventDefault();
+
+                            self.setState({browsingCategory: categoryName, searchValue: ''});
+                            $('.popup-scroll-area.emoji-one:visible').data('jsp').scrollTo(0);
+                        }}
+                    >
+                        <i className={"small-icon " + categoryIcons[categoryName]}></i>
                     </div>
                 );
             });
 
-            if (curCategoryEmojis.length > 0) {
-                emojis.push(
-                    <div key={categoryName}>
-                        {emojis.length > 0 ? <div className="clear"></div> : null}
-                        <div className="emoji-type-txt">
-                            {__(categoryName)}
-                        </div>
+            popupContents = <div>
+                <div className="popup-header emoji-one">
 
-                        <div className="clear"></div>
-                        {curCategoryEmojis}
-                        <div className="clear"></div>
+                    { preview ? preview : <div className="search-block emoji-one">
+                        <i className="small-icon search-icon"></i>
+                        <input type="search"
+                               placeholder={__("Search")}
+                               ref="emojiSearchField"
+                               onChange={this.onSearchChange}
+                               value={this.state.searchValue}/>
+
                     </div>
-                );
-            }
-        });
-        var categoryIcons = {
-            "FREQUENTLY USED": "clock-icon",
-            "PEOPLE": "smile-icon",
-            "NATURE": "sun-icon",
-            "FOOD & DRINK": "wineglass-icon",
-            "CELEBRATION": "present-icon",
-            "ACTIVITY": "bowling-ball-icon",
-            "TRAVEL & PLACES": "earth-icon",
-            "OBJECTS & SYMBOLS": "percents-icon"
-        };
+                    }
 
-        var categoryButtons = [];
 
-        Object.keys(categoryIcons).forEach(function(categoryName) {
-            var activeClass = self.state.browsingCategory === categoryName ? " active" : "";
-
-            if(
-                self.state.browsingCategory === false &&
-                self.state.searchValue === '' &&
-                categoryIcons[categoryName] === "clock-icon"
-            ) {
-                activeClass = " active";
-            }
-
-            categoryButtons.push(
-                <div
-                    className={"button square-button emoji-one" + (activeClass)}
-                    key={categoryIcons[categoryName]}
-                    onClick={(e) => {
-                        e.stopPropagation();
-                        e.preventDefault();
-
-                        self.setState({browsingCategory: categoryName, searchValue: ''});
-                        $('.popup-scroll-area.emoji-one:visible').data('jsp').scrollTo(0);
-                    }}
-                    >
-                    <i className={"small-icon " + categoryIcons[categoryName]}></i>
                 </div>
-            );
-        });
+
+
+
+                <utils.JScrollPane
+                    className="popup-scroll-area emoji-one"
+                    searchValue={this.state.searchValue}
+                    browsingCategory={this.state.browsingCategory}
+                >
+                    <div className="popup-scroll-content emoji-one">
+                        {emojis}
+                    </div>
+                </utils.JScrollPane>
+
+                <div className="popup-footer emoji-one">{categoryButtons}</div>
+            </div>;
+        } else {
+            popupContents = null;
+        }
 
         return <Dropdown className="popup emoji-one" {...self.props} ref="dropdown" onActiveChange={(newValue) => {
             // reset state if the dropdown is hidden
             if (newValue === false) {
                 self.setState(self.getInitialState);
+            } else {
+                self.setState({'isActive': true});
             }
         }}>
-            <div className="popup-header emoji-one">
-
-                { preview ? preview : <div className="search-block emoji-one">
-                    <i className="small-icon search-icon"></i>
-                    <input type="search"
-                           placeholder={__("Search")}
-                           ref="emojiSearchField"
-                           onChange={this.onSearchChange}
-                           value={this.state.searchValue}/>
-
-                </div>
-                }
-
-
-            </div>
-
-
-
-            <utils.JScrollPane
-                className="popup-scroll-area emoji-one"
-                searchValue={this.state.searchValue}
-                browsingCategory={this.state.browsingCategory}
-                >
-                <div className="popup-scroll-content emoji-one">
-                    {emojis}
-                </div>
-            </utils.JScrollPane>
-
-            <div className="popup-footer emoji-one">{categoryButtons}</div>
+            {popupContents}
         </Dropdown>;
     }
 });

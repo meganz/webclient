334e79bd95436e1513c1368cb2a6a13279880ea5
Lyubomir Petrov <lp@mega.co.nz>
2016-01-19 23:04:04 +0200

    second attempt to fix hanka's recursion problem;

--- js/crypto.js
+++ js/crypto.js
@@ -273,7 +273,6 @@ var crypt = (function () {
         if (
                 authring.hadInitialised() === false ||
                 typeof u_authring === 'undefined'
-                || typeof u_authring[keyType] === 'undefined'
         ) {
             // Need to initialise the authentication system (authring).
             logger.debug('Will wait for the authring to initialise first.', 'Tried to access: ', userhandle, keyType);

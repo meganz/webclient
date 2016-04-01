af385bb36e52ac0493f27f78f5db1a639c3f758b
Andrei Dymovich <ad@mega.co.nz>
2016-04-01 15:30:49 +0300

    4169 - Remove bandwidth for free accounts

--- index.js
+++ index.js
@@ -1126,6 +1126,7 @@ function topmenuUI() {
                 .safeHTML('<div class="membership-status @@">@@</div>', cssClass, purchasedPlan);
             $('.context-menu-divider.upgrade-your-account').addClass('pro');
             $('.membership-popup.pro-popup');
+            $('body').removeClass('free');
         }
         else {
             // Show the free badge
@@ -1133,6 +1134,7 @@ function topmenuUI() {
             $('.context-menu-divider.upgrade-your-account').removeClass('pro lite');
             $('.membership-status').addClass('free');
             $('.membership-status').text(l[435]);
+            $('body').addClass('free');
         }
 
         $('.membership-status').show();

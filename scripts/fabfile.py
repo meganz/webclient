cc2bf7a497a34c6761e031f86cb43146970197bd
Lyubomir Petrov <lp@mega.co.nz>
2016-04-12 14:45:09 +0300

    updated fabfile to always remove the bundle.js and then to check it out back from the repo (in case it was rebuilt manually on the server, this would simply revert to the bundle.js version from the repo);

--- scripts/fabfile.py
+++ scripts/fabfile.py
@@ -130,8 +130,11 @@ def dev(build_bundle=False, branch_name=''):
         else:
             print('Branch already exists on beta, updating instead.\n')
             with cd(remote_branch_path):
+                # Force revert the bundle.js IF it was re-built in earlier deployments
+                run('rm -rf js/chat/bundle.js')
                 run('git pull --update-shallow')
                 run('git log -1')
+                run('git checkout js/chat/bundle.js')
 
         # Update version info.
         version = None

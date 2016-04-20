4704b4dadf7651cdce15c50af60b691410e7f293
Chris <csr@mega.co.nz>
2016-04-20 17:58:39 +1200

    4119: Added some retina images for the checkbox and two log messages for the plan expiry dialog

--- js/ui/alarm.js
+++ js/ui/alarm.js
@@ -284,6 +284,9 @@ var alarm = {
 
                 // Set a flag so it doesn't show each time
                 alarm.seen = true;
+                
+                // Add a log
+                api_req({ a: 'log', e: 99606, m: 'User chose a new plan from the plan expiry dialog' });
 
                 // Go to the first step of the Pro page so they can choose a new plan
                 document.location.hash = 'pro';
@@ -305,6 +308,9 @@ var alarm = {
 
                 // Set a flag so it doesn't show each time
                 alarm.seen = true;
+                
+                // Add a log
+                api_req({ a: 'log', e: 99607, m: 'User chose to renew existing plan from the plan expiry dialog' });
 
                 // Get the link for the Pro page second step e.g. #pro_lite, #pro_1 etc
                 var proLink = (proNum === 4) ? 'lite' : proNum;

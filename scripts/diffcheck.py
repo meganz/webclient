f5662689253cb192b50301b3e5dcf8e84db0487a
Diego Casorran <dc@mega.co.nz>
2016-06-30 06:54:57 +0200

    Make jshint ignore jsx files...

--- scripts/diffcheck.py
+++ scripts/diffcheck.py
@@ -131,7 +131,7 @@ def reduce_jshint(file_line_mapping, **extra):
     rules = config.JSHINT_RULES if not norules else ''
     files_to_test = [os.path.join(*x)
                      for x in file_line_mapping.keys()
-                     if x[-1].split('.')[-1] in ['js', 'jsx']]
+                     if x[-1].split('.')[-1] in ['js']]
 
     if len(files_to_test) == 0:
         logging.info('JSHint: No modified JavaScript files found.')

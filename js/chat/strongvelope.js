a921fd6d6718f3a35698cfa6f907da0396aa0c4a
vincent <guoy@vinents-MacBook-Pro.local>
2016-07-26 10:36:25 +1200

    make sure to convert the message ids from any client to be string.

--- js/chat/strongvelope.js
+++ js/chat/strongvelope.js
@@ -572,7 +572,7 @@ var strongvelope = {};
                 return false;
             }
 
-            var identity = payload.substr(0, MESSAGE_IDENTITY_SIZE);
+            var identity = asmCrypto.bytes_to_string(payload.substr(0, MESSAGE_IDENTITY_SIZE));
             var refidlen = ns.unpack16le(payload.substr(MESSAGE_IDENTITY_SIZE, MESSAGE_REFERENCE_SIZE));
             var refidstr = payload.substr(MESSAGE_IDENTITY_SIZE + MESSAGE_REFERENCE_SIZE, refidlen);
             var refids = [];

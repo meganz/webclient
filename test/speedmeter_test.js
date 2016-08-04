14ba69c927dba3dd1ecc7c06f30f0407a3192c58
Cesar Rodas <cr@mega.co.nz>
2016-08-04 16:02:33 -0400

    Improve SpeedMeter test

--- test/speedmeter_test.js
+++ test/speedmeter_test.js
@@ -1,9 +1,17 @@
+/**
+ *  If progress was called right away when the SpeedMeter object
+ *  was created (only reproducible if you have a very fast
+ *  internet connection) it ended up dividing the speed by 0 (which is Infinity).
+ */
 describe("test speedmeter bug with fast connections", function(next) {
     'use strict';
 
     var sp = new SpeedMeter(function() {});
     var mb5 = 5 * 1024 * 1024;
     sp.progress(0, mb5 * 5);
+    for (i = 0; i < 500; ++i) {
+        sp.progress(i * 2, mb5 * 5);
+    }
     var i = 0;
     var tick = setInterval(function() {
         sp.progress(mb5*(++i));

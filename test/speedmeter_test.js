a2ea3e6003731342fb668f1f0fd0af78d7827191
Cesar Rodas <cr@mega.co.nz>
2016-08-04 16:07:30 -0400

    Improve tests

--- test/speedmeter_test.js
+++ test/speedmeter_test.js
@@ -3,22 +3,26 @@
  *  was created (only reproducible if you have a very fast
  *  internet connection) it ended up dividing the speed by 0 (which is Infinity).
  */
-describe("test speedmeter bug with fast connections", function(next) {
+describe("test speedmeter bug with fast connections", function() {
     'use strict';
 
-    var sp = new SpeedMeter(function() {});
-    var mb5 = 5 * 1024 * 1024;
-    sp.progress(0, mb5 * 5);
-    for (i = 0; i < 500; ++i) {
-        sp.progress(i * 2, mb5 * 5);
-    }
-    var i = 0;
-    var tick = setInterval(function() {
-        sp.progress(mb5*(++i));
+    it("doesn't show Infinity", function(next) {
+        var sp = new SpeedMeter(function() {});
+        var mb5 = 5 * 1024 * 1024;
+        sp.progress(0, mb5 * 5);
         assert(sp.getData().speed.match(/infin/i) === null);
-        if (i == 5) {
-            clearInterval(tick);
-            next();
+        for (i = 0; i < 500; ++i) {
+            sp.progress(i * 2, mb5 * 5);
+            assert(sp.getData().speed.match(/infin/i) === null);
         }
-    }, 1000);
+        var i = 0;
+        var tick = setInterval(function() {
+            sp.progress(mb5*(++i));
+            assert(sp.getData().speed.match(/infin/i) === null);
+            if (i == 5) {
+                clearInterval(tick);
+                next();
+            }
+        }, 100);
+    });
 });

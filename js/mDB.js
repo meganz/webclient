feecf4911426ecd42d1d5c509b1c7e6931d8bbe7
Diego Casorran <dc@mega.co.nz>
2017-01-19 04:06:31 +0100

    recreate fmdb

--- js/mDB.js
+++ js/mDB.js
@@ -102,7 +102,7 @@ FMDB.prototype.init = function fmdb_init(result, wipe) {
         try {
             if (!fmdb.db) {
                 var todrop = [];
-                var dbpfx = 'fm4_';
+                var dbpfx = 'fm5_';
 
                 // enumerate databases and collect those not prefixed with 'dbpfx'
                 // (which is the current format)

bbf687eb72ff35b759d0a8b3007396fd7005567b
Diego Casorran <dc@mega.nz>
2017-05-05 03:57:58 +0200

    #6606: Copying file from Cloud Drive opens a dialog with a permissions button (on Incoming shares tab)

--- js/fm.js
+++ js/fm.js
@@ -5868,7 +5868,7 @@ function handleDialogContent(dialogTabClass, parentTag, newFolderButton, dialogP
     var html,
         $btn = '';// Action button label
 
-    if ($.onImportCopyNodes && (!newFolderButton || (dialogPrefix !== 'copy'))) {
+    if (($.copyToShare || $.onImportCopyNodes) && (!newFolderButton || (dialogPrefix !== 'copy'))) {
 
         // XXX: Ideally show some notification that importing from folder link to anything else than the cloud isn't supported.
         $('.copy-dialog-button.' + String(dialogTabClass).replace(/[^\w-]/g, '')).fadeOut(200).fadeIn(100);
@@ -5935,7 +5935,7 @@ function handleDialogContent(dialogTabClass, parentTag, newFolderButton, dialogP
     }
 
     // If copying from contacts tab (Ie, sharing)
-    if (buttonLabel === l[1344]) {
+    if (dialogTabClass === 'cloud-drive' && M.currentrootid === 'contacts') {
         $('.fm-dialog.copy-dialog .share-dialog-permissions').removeClass('hidden');
         $('.dialog-newfolder-button').addClass('hidden');
         $('.copy-operation-txt').text(l[1344]);
@@ -6908,6 +6908,7 @@ function closeDialog() {
 
         delete $.copyDialog;
         delete $.moveDialog;
+        delete $.copyToShare;
         delete $.copyrightsDialog;
     }
     $('.fm-dialog').removeClass('arrange-to-back');
@@ -7170,14 +7171,9 @@ function copyDialog() {
         var itemTopPos = $item.offset().top;
         var $tooltip = $('.copy-dialog .contact-preview');
         var sharedNodeHandle = $(this).attr('id').replace('mctreea_', '');
-        var ownerHandle = M.d[sharedNodeHandle].u;
-        var ownerEmail = M.u[ownerHandle].m;
-        var ownerName = M.u[ownerHandle].name;
-
-        // Not allowing undefined to be shown like owner name
-        if (typeof ownerName === 'undefined') {
-            ownerName = '';
-        }
+        var ownerHandle = sharer(sharedNodeHandle);
+        var ownerEmail = Object(M.u[ownerHandle]).m || '';
+        var ownerName = Object(M.u[ownerHandle]).name || '';
 
         var html = useravatar.contact(ownerHandle, 'small-rounded-avatar', 'div') +
             '<div class="user-card-data no-status">' +
@@ -7188,6 +7184,7 @@ function copyDialog() {
 
         $tooltip.find('.contacts-info.body').safeHTML(html);
 
+        clearTimeout(copyDialogTooltipTimer);
         copyDialogTooltipTimer = setTimeout(function () {
             $tooltip.css({
                 'left': itemLeftPos + (($item.outerWidth() / 2) - ($tooltip.outerWidth() / 2))  + 'px',
@@ -7195,6 +7192,8 @@ function copyDialog() {
             });
             $tooltip.fadeIn(200);
         }, 200);
+
+        return false;
     });
 
     $('.copy-dialog .shared-with-me').off('mouseleave', '.nw-fm-tree-item');
@@ -7204,6 +7203,8 @@ function copyDialog() {
 
         clearTimeout(copyDialogTooltipTimer);
         $tooltip.hide();
+
+        return false;
     });
 
     // Handle conversations tab item selection
@@ -7519,9 +7520,9 @@ function moveDialog() {
         var itemTopPos = $item.offset().top;
         var $tooltip = $('.move-dialog .contact-preview');
         var sharedNodeHandle = $(this).attr('id').replace('mctreea_', '');
-        var ownerHandle = M.d[sharedNodeHandle].u;
-        var ownerEmail = M.u[ownerHandle].m;
-        var ownerName = M.u[ownerHandle].name;
+        var ownerHandle = sharer(sharedNodeHandle);
+        var ownerEmail = Object(M.u[ownerHandle]).m || '';
+        var ownerName = Object(M.u[ownerHandle]).name || '';
 
         // Not allowing undefined to be shown like owner name
         if (typeof ownerName === 'undefined') {
@@ -7537,6 +7538,7 @@ function moveDialog() {
 
         $tooltip.find('.contacts-info.body').safeHTML(html);
 
+        clearTimeout(moveDialogTooltipTimer);
         moveDialogTooltipTimer = setTimeout(function () {
             $tooltip.css({
                 'left': itemLeftPos + (($item.outerWidth() / 2) - ($tooltip.outerWidth() / 2))  + 'px',
@@ -7544,6 +7546,8 @@ function moveDialog() {
             });
             $tooltip.fadeIn(200);
         }, 200);
+
+        return false;
     });
 
     $('.move-dialog .shared-with-me').off('mouseleave', '.nw-fm-tree-item');
@@ -7553,6 +7557,8 @@ function moveDialog() {
 
         clearTimeout(moveDialogTooltipTimer);
         $tooltip.hide();
+
+        return false;
     });
 
     $('.move-dialog .dialog-move-button').rebind('click', function() {
@@ -9283,6 +9289,7 @@ function contactUI() {
 
             $.copyDialog = 'copy';
             $.mcselected = undefined;
+            $.copyToShare = true;
 
             handleDialogContent('cloud-drive', 'ul', true, 'copy', l[1344]);
             fm_showoverlay();

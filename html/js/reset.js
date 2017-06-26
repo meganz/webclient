de31eeb205bbf3f5d3ff0a9581458349184ba625
Bram van der Kolk <bk@mega.co.nz>
2017-06-26 16:52:32 +1200

    Fix "mainScroll" JS bug

--- html/js/reset.js
+++ html/js/reset.js
@@ -300,8 +300,6 @@ function reset_pwcheck() {
 
 function init_reset_pw() {
     var a = '';
-
-    Soon(mainScroll);
     $('#withkey-password,#withoutkey-password').rebind('focus', function(e) {
         $('.login-register-input.password.first').removeClass('incorrect');
         $('.login-register-input.password.confirm').removeClass('incorrect');

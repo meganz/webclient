meganz/webclient

commit 665bfd0696b01dba6388c942ba7d9d6cb0789bba

Merging fix for the previous merge conflicts of: js/attr.js

js/attr.js
@@ -531,10 +531,10 @@ var attribCache = false;
 
         var proxyPromise = new MegaPromise();
         proxyPromise.always(function() {
-            logger.debug("finished: ", proxyPromise.state(), self.toString());
+            // logger.debug("finished: ", proxyPromise.state(), self.toString());
 
             self.state = QueuedSetArrayAttribute.STATE.DONE;
-            removeValue(self.attr._queuedSetArrayAttributeOps, self);
+            array.remove(self.attr._queuedSetArrayAttributeOps, self);
 
             if (self.attr._queuedSetArrayAttributeOps.length > 0) {
                 // execute now

commit 3365016a7a5606365ae193264870b2b6158e44a3

Proper fix of the issues caused in 665bfd0696b01dba6388c942ba7d9d6cb0789bba and 709b8fa84cf10f0e869200e8f420afe7adc6e2b1 and revert of Mathias's fix.

js/attr.js
@@ -622,7 +622,7 @@ var attribCache = false;
             var serializedValue = arr;
 
             proxyPromise.linkDoneAndFailTo(
-                self.set(
+                self.attr.set(
                     self.attributeName,
                     serializedValue,
                     self.pub,
@@ -637,22 +637,28 @@ var attribCache = false;
 
         self.attr.get(u_handle, self.attributeName, self.pub, self.nonHistoric)
             .done(function(r) {
-                if (r === -9) {
-                    _setArrayAttribute({});
-                    proxyPromise.reject(r);
-                }
-                else {
-                    try {
+                try {
+                    if (r === -9) {
+                        _setArrayAttribute({});
+                        proxyPromise.reject(r);
+                    }
+                    else {
                         _setArrayAttribute(r);
                     }
-                    catch (e) {
-                        proxyPromise.reject(e, r);
-                    }
+                }
+                catch (e) {
+                    logger.error("QueuedSetArrayAttribute failed, because of exception: ", e);
+                    proxyPromise.reject(e, r);
                 }
             })
             .fail(function(r) {
                 if (r === -9) {
-                    _setArrayAttribute({});
+                    try {
+                        _setArrayAttribute({});
+                    }
+                    catch (e) {
+                        logger.error("QueuedSetArrayAttribute failed, because of exception: ", e);
+                    }
                     proxyPromise.reject(r);
                 }
                 else {
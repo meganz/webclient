c7cf4915157f56db69ab70f62c0e49a09509d383
Diego Casorran <dc@mega.nz>
2017-08-20 05:56:23 +0200

    #5866: Downloads resume support.

--- js/fm/megadata/transfers.js
+++ js/fm/megadata/transfers.js
@@ -553,6 +553,8 @@ MegaData.prototype.dlcomplete = function(dl) {
     var id = dl.id, z = dl.zipid;
     var $overlay = $('.viewer-overlay');
 
+    dlmanager.remResumeInfo(dl).dump();
+
     if (slideshowid == id && !previews[slideshowid]) {
         $overlay.find('.viewer-pending').addClass('hidden');
         $overlay.find('.viewer-error').addClass('hidden');

ccdb453702610d6b25ce66c0c82e5f47b1a9257e
Lyubomir Petrov <lp@mega.co.nz>
2017-12-04 14:50:50 +0200

    #8092 - Firstname is truncated if it includes spaces

--- js/chat/bundle.js
+++ js/chat/bundle.js
@@ -8674,8 +8674,13 @@ React.makeElement = React['createElement'];
 
 	        if (Object.keys(self.state.currentlyTyping).length > 0) {
 	            var names = Object.keys(self.state.currentlyTyping).map(function (u_h) {
-	                var avatarMeta = generateAvatarMeta(u_h);
-	                return avatarMeta.fullName.split(" ")[0];
+	                var contact = M.u[u_h];
+	                if (contact && contact.firstName) {
+	                    return contact.firstName;
+	                } else {
+	                    var avatarMeta = generateAvatarMeta(u_h);
+	                    return avatarMeta.fullName.split(" ")[0];
+	                }
 	            });
 
 	            var namesDisplay = "";

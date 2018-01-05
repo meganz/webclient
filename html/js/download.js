463f7b903f0f6d56a3fa7a6fd9dfcf7e59b96c47
Diego Casorran <dc@mega.nz>
2018-01-05 08:03:45 +0100

    safari fixup.

--- html/js/download.js
+++ html/js/download.js
@@ -433,7 +433,7 @@ function dl_g(res) {
             if (res.fa) {
                 var promise = Promise.resolve();
 
-                if (String(res.fa).indexOf(':8*') > 0) {
+                if (!window.safari && String(res.fa).indexOf(':8*') > 0) {
                     promise = MediaAttribute.canPlayMedia(dl_node);
                     prevBut = false;
                 }

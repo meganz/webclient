d2d8034a93176040ff693a944e0aa0284152dce6
Lyubomir Petrov <lp@mega.co.nz>
2018-03-16 15:53:46 +0200

    #8825, #8824 - Don't show desktop notifications (but update counters and stuff) for non-NEWMSG messages; Investigate potential wrong/not set LAST SEEN

--- js/chat/bundle.js
+++ js/chat/bundle.js
@@ -11556,6 +11556,8 @@ React.makeElement = React['createElement'];
 	        }
 	    }
 
+	    message.source = Message.SOURCE.SENT;
+
 	    self.trigger('onMessageAppended', message);
 	    self.messagesBuff.messages.push(message);
 

f63b9fff345b6853273b17e14196cd5cfbf8dbf5
Lyubomir Petrov <lp@mega.co.nz>
2018-03-27 19:11:43 +0300

    Clear chat causes multi tabs to still show 1 unread in one of the multi tabs (the master one).

--- js/chat/messages.js
+++ js/chat/messages.js
@@ -1161,7 +1161,13 @@ var MessagesBuff = function(chatRoom, chatdInt) {
     self.addChangeListener(function() {
         var newCounter = 0;
         self.messages.forEach(function(v, k) {
-            if (v.getState && v.getState() === Message.STATE.NOT_SEEN && !v.deleted && v.textContents !== "") {
+            if (
+                v.getState &&
+                v.getState() === Message.STATE.NOT_SEEN &&
+                !v.deleted &&
+                v.textContents !== "" &&
+                v.textContents /* not false-based value */
+            ) {
                 var shouldRender = true;
                 if (
                     (v.isManagement && v.isManagement() === true && v.isRenderableManagement() === false) ||

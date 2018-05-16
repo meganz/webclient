6031343256c5a9dcca748e29c067c48bf9f926df
Khaled Daifallah <kd@mega.co.nz>
2018-05-17 11:58:25 +1200

    #9486: building APi Business Class - step1 - 2

---
+++ js/fm/megadata/businessaccount.js
@@ -0,0 +1,156 @@
+﻿/**
+ * a class to apply actions on business account in collaboration with API
+ */
+function BusinessAccount() {
+
+}
+
+/**
+ * Function to add sub user to a business account
+ * @param {String} subEmail  email address of new user
+ * @param {String} subFName  First name of new user
+ * @param {String} subLName  Last name of new user
+ * @returns {Promise}        Resolves with new add user HANDLE
+ */
+BusinessAccount.prototype.addSubAccount = function _addSubAccount(subEmail, subFName, subLName) {
+    var operationPromise = new MegaPromise();
+    if (checkMail(subEmail)) {
+        // promise reject/resolve will return: success,errorCode,errorDesc
+        return operationPromise.reject(0, 1,'Invalid Email');
+    }
+    if (!subFName) {
+        return operationPromise.reject(0, 2, 'Empty First Name');
+    }
+    if (!subLName) {
+        return operationPromise.reject(0, 3, 'Empty Last Name');
+    }
+    var request = {
+        "a": "sbu", // business sub account operation
+        "aa": "a", // add operation
+        "m": subEmail, // email address of user to add
+        "fn": subFName, // first name of user to add (not base64 encoded like attributes are)
+        "ln": subLName // last name of user to add (also not base64 encoded)
+    };
+
+    api_req(request, {
+        callback: function (res) {
+            if ($.isNumeric(res)) {
+                operationPromise.reject(0, res, 'API returned error');
+            }
+            else if (typeof res === 'string') {
+                operationPromise.resolve(1, res); // new added user handle
+            }
+            else {
+                operationPromise.reject(0, 4, 'API returned error, ret=' + res);
+            }
+        }
+    });
+
+    return operationPromise;
+};
+
+/**
+ * Function to deactivate sub user from business account
+ * @param {String} subUserHandle    sub user handle to deactivate
+ * @returns {Promise}               Resolves deactivate opertation result
+ */
+BusinessAccount.prototype.deActivateSubAccount = function _deActivateSubAccount(subUserHandle) {
+    var operationPromise = new MegaPromise();
+    if (!subUserHandle || subUserHandle.length !== 11) {
+        return operationPromise.reject(0, 5, 'invalid U_HANDLE');
+    }
+    var request = {
+        "a": "sbu", // business sub account operation
+        "aa": "d", // deactivate operation
+        "u": subUserHandle // user handle to deactivate
+    };
+
+    api_req(request, {
+        callback: function (res) {
+            if ($.isNumeric(res)) {
+                if (res !== 0) {
+                    operationPromise.reject(0, res, 'API returned error');
+                }
+                else {
+                    operationPromise.resolve(1); // user deactivated successfully
+                }
+            }
+            else {
+                operationPromise.reject(0, 4, 'API returned error, ret=' + res);
+            }
+        }
+
+    });
+
+    return operationPromise;
+};
+
+/**
+ * Function to activate sub user in business account
+ * @param {String} subUserHandle    sub user handle to activate
+ * @returns {Promise}               Resolves activate opertation result
+ */
+BusinessAccount.prototype.activateSubAccount = function _activateSubAccount(subUserHandle) {
+    var operationPromise = new MegaPromise();
+    if (!subUserHandle || subUserHandle.length !== 11) {
+        return operationPromise.reject(0, 5, 'invalid U_HANDLE');
+    }
+    var request = {
+        "a": "sbu", // business sub account operation
+        "aa": "c", // activate operation
+        "u": subUserHandle // user handle to activate
+    };
+
+    api_req(request, {
+        callback: function (res) {
+            if ($.isNumeric(res)) {
+                if (res !== 0) {
+                    operationPromise.reject(0, res, 'API returned error');
+                }
+                else {
+                    operationPromise.resolve(1); // user activated successfully
+                }
+            }
+            else {
+                operationPromise.reject(0, 4, 'API returned error, ret=' + res);
+            }
+        }
+
+    });
+
+    return operationPromise;
+};
+
+/**
+ * Function to get the master key of a sub user in business account
+ * @param {String} subUserHandle    sub user handle to get the master key of
+ * @returns {Promise}               Resolves opertation result
+ */
+BusinessAccount.prototype.getSubAccountMKey = function _getSubAccountMKey(subUserHandle) {
+    var operationPromise = new MegaPromise();
+    if (!subUserHandle || subUserHandle.length !== 11) {
+        return operationPromise.reject(0, 5, 'invalid U_HANDLE');
+    }
+    var request = {
+        "a": "sbu", // business sub account operation
+        "aa": "k", // get master-key operation
+        "u": subUserHandle // user handle to get key of
+    };
+
+    api_req(request, {
+        callback: function (res) {
+            if ($.isNumeric(res)) {
+                operationPromise.reject(0, res, 'API returned error');
+            }
+            else if (typeof res === 'string') {
+                operationPromise.resolve(1, res); // sub-user master-key
+            }
+            else {
+                operationPromise.reject(0, 4, 'API returned error, ret=' + res);
+            }
+        }
+
+    });
+
+    return operationPromise;
+};

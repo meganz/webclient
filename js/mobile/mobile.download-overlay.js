243de36d472d3bbbd367076551a28194517bb44f
Ct <ct@mega.co.nz>
2018-05-17 19:15:40 +1200

    jenkins fix

--- js/mobile/mobile.download-overlay.js
+++ js/mobile/mobile.download-overlay.js
@@ -70,14 +70,13 @@ mobile.downloadOverlay = {
         }
 
         // On Open in Browser button click/tap
-
         this.$overlay.find('.second.dl-browser').off('tap').on('tap', function() {
        
-                // Start the download
-                mobile.downloadOverlay.startFileDownload(nodeHandle);
+            // Start the download
+            mobile.downloadOverlay.startFileDownload(nodeHandle);
 
-                // Prevent default anchor link behaviour
-                return false;
+            // Prevent default anchor link behaviour
+            return false;
             
         });
     },

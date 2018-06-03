228a304ae058f317c9b7e6cfd79418fab123b609
Khaled Daifallah <kd@mega.co.nz>
2018-06-04 00:38:12 +1200

    #9486: Business Account - Step 9

---
+++ js/fm/businessAccountUI.js
@@ -0,0 +1,39 @@
+﻿
+/**
+ * A UI control Class to perfrom Business Account related UI
+ */
+function BusinessAccountUI() {
+    
+}
+
+/**
+ * Function to view the right pane of "Users Management" used by master users to manage sub-accounts
+ * @param {string[]} subAccounts    optional list of subAccount, Default is M.suba
+ * @param {boolean} isBlockView     by default "Grid" view will be used, this param when True will change to "Block"
+ * @returns {boolean}               true if OK, false if something went wrong
+ */
+BusinessAccountUI.prototype.viewSubAccountListUI = function _viewSubAccountListUI(subAccounts, isBlockView) {
+    if (!M.isBusinessAccountMaster) {
+        return false;
+    }
+    if (!subAccounts) {
+        if (M.suba) {
+            subAccounts = M.suba;
+        }
+        else {
+            return false;
+        }
+    }
+
+    // hide any possible grid or block view.
+    $('.files-grid-view, .fm-blocks-view').addClass('hidden');
+
+    var subAccountsView;
+    if (!isBlockView) {
+        subAccountsView = $('.files-grid-view.user-management-view');
+    }
+    else {
+        subAccountsView = $('.fm-blocks-view.user-management-view');
+    }
+
+};

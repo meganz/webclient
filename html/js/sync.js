07f7d93ce9cb3502b3ab7f9cbbc51ca95bfcf9ee
csr <csr@mega.nz>
2018-10-24 20:52:44 -0300

    Preload linux options if on Linux

--- html/js/sync.js
+++ html/js/sync.js
@@ -75,6 +75,11 @@ function initMegasync() {
 
     resetMegasync();
 
+    // Preload linux options if on a linux client
+    if (ua.indexOf('linux') >= 0) {
+        megasync.getLinuxReleases(renderLinuxOptions);
+    }
+
     $content.find('.nav-buttons-bl a').rebind('click', function() {
         var $this = $(this);
         var osData = $this.attr('data-os');

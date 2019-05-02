1407d4c5622a50514545ff90dff90ee008427100
Lyubomir Petrov <lp@mega.co.nz>
2019-05-02 09:55:45 +0000

    Small bug and performance fixes

--- js/chat/ui/conversationaudiovideopanel.jsx
+++ js/chat/ui/conversationaudiovideopanel.jsx
@@ -907,7 +907,7 @@ var ConversationAudioVideoPanel = React.createClass({
                         {Object.keys(chatRoom.callParticipants).length}
                     </div>
 
-                    <a href="javascript:;" className={
+                    <a className={
                         "call-switch-view " + (self.getViewMode() === VIEW_MODES.GRID ? " grid" : " carousel") +
                         (participantsCount > MAX_PARTICIPANTS_FOR_GRID_MODE ? " disabled" : "")
                     } onClick={function(e) {
@@ -927,7 +927,7 @@ var ConversationAudioVideoPanel = React.createClass({
                         videoSessionCount >= RtcModule.kMaxCallVideoSenders ? " limit-reached" : ""
                     )}>{videoSessionCount} / {RtcModule.kMaxCallVideoSenders}</div>
 
-                    <div 
+                    <div
                         className={
                             "call-video-icon" + (
                                 chatRoom.callManagerCall.hasVideoSlotLimitReached() ? " call-video-icon-warn" : ""

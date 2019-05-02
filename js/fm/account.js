877ad9ea993d5c75d1e305e7d2330d23e5e83baa
Joon Shin <jsh@mega.co.nz>
2019-05-03 10:09:57 +1200

    4749: Feature: Show disabled Grant bandwidth slider for free account

--- js/fm/account.js
+++ js/fm/account.js
@@ -2762,7 +2762,15 @@ accountUI.transfers = {
                         }
                     });
                     $('.slider-percentage span').text(bandwidthLimit + ' %');
-                    $('.bandwith-settings').removeClass('hidden');
+                    $('.bandwith-settings').removeClass('disabled').addClass('border');
+                    $('.slider-percentage-bl').removeClass('hidden');
+                    $('.band-grn-noti').addClass('hidden');
+                }
+                // Business account
+                else if (u_attr.b) {
+                    $('.bandwith-settings').addClass('hidden');
+                    $('.slider-percentage-bl').addClass('hidden');
+                    $('.band-grn-noti').addClass('hidden');
                 }
             }
         },

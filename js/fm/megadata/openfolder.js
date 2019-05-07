c4070fe16b2409626607f5a71bac38ff91637526
Lyubomir Petrov <lp@mega.co.nz>
2019-05-07 14:49:59 +0000

    #11985 - In Archived chat page when the browser is refreshed it throws Chat link unavailable dialog

--- js/fm/megadata/openfolder.js
+++ js/fm/megadata/openfolder.js
@@ -241,7 +241,7 @@
             newHashLocation = 'F!' + pfid + '!' + pfkey + target;
             this.lastSeenFolderLink = newHashLocation;
         }
-        else if (id && (id.startsWith('chat/') && id[6] !== '/')) {
+        else if (id && id !== "chat/archived" && (id.startsWith('chat/') && id[6] !== '/')) {
             // is a chat link, e.g. chat/[^/]
             newHashLocation = this.currentdirid;
         }

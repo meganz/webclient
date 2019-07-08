9367b508cac58f1f3f5fd106b29c1e29f6fabfe1
Khaled Daifallah <kd@mega.co.nz>
2019-07-08 13:31:33 +1200

    #12602: Bugfix: Change menu items order of sorting in grid view & hiding some items & fixing saved and default applied sort

--- js/fm/filemanager.js
+++ js/fm/filemanager.js
@@ -3171,21 +3171,6 @@ FileManager.prototype.addGridUI = function(refresh) {
             for (var sortBy in M.sortRules) {
                 if (cls.indexOf(sortBy) !== -1) {
 
-                    var $sortMenu = $('.files-menu.context .submenu.sorting .dropdown-item.sort-grid-item')
-                        .removeClass('selected');
-                    var sortitem = sortBy;
-
-                    if (sortitem === 'ts') {
-                        sortitem = 'timeAd';
-                    }
-                    else if (sortitem === 'mtime') {
-                        sortitem = 'timeMd';
-                    }
-                    else if (sortitem === 'date') {
-                        sortitem = 'sharecreated, .sort-timeAd';
-                    }
-
-                    $sortMenu.filter('.sort-' + sortitem).addClass('selected');
 
                     if (dir !== -1 && sortitem !== sortBy) {
                         if (cls.indexOf('asc') === -1) {

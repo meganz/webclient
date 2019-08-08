5992d8872b773c1bc77835c6d1f909b8675e5c20
Alexander Vassilev <av@mega.co.nz>
2019-08-08 23:16:00 +0300

    Fix remote audio not heard in audio-only calls

--- js/chat/webrtc.js
+++ js/chat/webrtc.js
@@ -2982,7 +2982,7 @@ Session.prototype._createRtcConn = function() {
             // Audio level notifications for all peers seems to require too much CPU
             // self.audioLevelMonitor = new AudioLevelMonitor(stream, self.handler);
         }
-        if (self.state !== SessState.kSessInProgress) {
+        if (self.state !== SessState.kConnecting && self.state !== SessState.kSessInProgress) {
             return;
         }
         // Just in case, check if the actual stream a/v tracks match the ones negotiated

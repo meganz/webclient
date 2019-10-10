130346a7c88fcc95982b6e1a5570356c7a761b7d
Andrei Dymovich <ad@mega.co.nz>
2019-10-10 15:39:30 +0000

    #13241 and #13366: Design changes on product pages, "angle" bg bug fix

--- js/utils/locale.js
+++ js/utils/locale.js
@@ -909,6 +909,8 @@ mBroadcaster.once('startMega', function populate_l() {
     l[20975] = escapeHTML(l[20975]).replace('[B]', '<b class="txt-dark">').replace('[/B]', '</b>')
         .replace('[A]', '<a href="/security" class="red txt-bold" target="_blank">').replace('[/A]', '</a>');
     l[22074] = l[22074].replace('[S]', '<span class="purchase">').replace('[/S]', '</span>');
+    l[22094] = l[22094].replace(/\[S\]/g, '<strong>').replace(/\[\/S\]/g, '</strong>');
+    l[22095] = l[22095].replace(/\[S\]/g, '<strong>').replace(/\[\/S\]/g, '</strong>');
 
     var common = [
         15536, 16106, 16107, 16119, 16120, 16123, 16124, 16135, 16136, 16137, 16138, 16304, 16313, 16315, 16316,

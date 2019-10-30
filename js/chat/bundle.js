704fd2ee86bfde55c4c0d296ffb8183fd910b48d
Alexander Vassilev <av@mega.co.nz>
2019-10-30 15:02:55 +0200

    Sort contacts in contact picker by name

--- js/chat/bundle.js
+++ js/chat/bundle.js
@@ -2184,6 +2184,10 @@ function (_MegaRenderMixin9) {
       self.props.contacts.forEach(function (v, k) {
         !alreadyAdded[v.h] && self._eventuallyAddContact(v, contacts, selectableContacts);
       });
+      var sortFn = M.getSortByNameFn2(1);
+      contacts.sort(function (a, b) {
+        return sortFn(a.props.contact, b.props.contact);
+      });
 
       if (Object.keys(alreadyAdded).length === 0) {
         hideFrequents = true;
@@ -6340,7 +6344,6 @@ function (_MegaRenderMixin5) {
       }, React.makeElement("input", {
         type: "text",
         className: "chat-quick-search",
-        autoComplete: "disabled",
         onChange: function onChange(e) {
           if (e.target.value) {
             treesearch = e.target.value;
@@ -6355,6 +6358,7 @@ function (_MegaRenderMixin5) {
             treesearch = e.target.value;
           }
         },
+        autoComplete: "disabled",
         value: self.state.quickSearchText,
         placeholder: l[7997]
       }), React.makeElement("div", {
@@ -9165,7 +9169,7 @@ function (_MegaRenderMixin) {
           return;
         }
 
-        var _char = String.fromCharCode(key);
+        var char = String.fromCharCode(key);
 
         if (self.prefillMode) {
           return; // halt next checks if its in prefill mode.
@@ -9191,7 +9195,7 @@ function (_MegaRenderMixin) {
         /* up */
         || key === 9
         /* tab */
-        || _char.match(self.validEmojiCharacters)) {
+        || char.match(self.validEmojiCharacters)) {
           var parsedResult = mega.utils.emojiCodeParser(currentContent, currentCursorPos);
           self.setState({
             'emojiSearchQuery': parsedResult[0],
@@ -10954,7 +10958,7 @@ function (_React$Component) {
         var result = audio.play();
 
         if (result instanceof Promise) {
-          result["catch"](function (e) {
+          result.catch(function (e) {
             console.error(e);
           });
         }
@@ -11237,7 +11241,7 @@ function (_React$Component) {
               loading: false
             };
           });
-        })["catch"](function (e) {
+        }).catch(function (e) {
           console.error(e);
         });
       }
@@ -11642,7 +11646,7 @@ function (_ConversationMessageM) {
                 console.warn('Unable to inject nodes... no longer existing?', res);
               }
             });
-          })["catch"](function () {
+          }).catch(function () {
             if (d) {
               console.error("Failed to allocate 'My chat files' folder.", arguments);
             }
@@ -17724,7 +17728,7 @@ var webSocketsSupport = typeof WebSocket !== 'undefined';
       var userHandles = [u_handle, userHandle];
       megaChat.smartOpenChat(userHandles, "private", undefined, undefined, undefined, true).then(function (room) {
         room.show();
-      })["catch"](function (ex) {
+      }).catch(function (ex) {
         console.warn("openChat failed. Maybe tried to start a private chat with a non contact?", ex);
       });
     } else if (roomType === "group") {
@@ -18888,7 +18892,7 @@ Chat.prototype.smartOpenChat = function () {
         return ready();
       }
 
-      createTimeoutPromise(verify, 300, 3e4).then(ready)["catch"](reject);
+      createTimeoutPromise(verify, 300, 3e4).then(ready).catch(reject);
     }; // Check whether we can prevent the actual call to openChat()
 
 
@@ -18905,7 +18909,7 @@ Chat.prototype.smartOpenChat = function () {
 
     if (result instanceof MegaPromise) {
       // if an straight promise is returned, the operation got rejected
-      result.then(reject)["catch"](reject);
+      result.then(reject).catch(reject);
     } else if (!Array.isArray(result)) {
       // The function should return an array at all other times...
       reject(EINTERNAL);
@@ -18928,7 +18932,7 @@ Chat.prototype.smartOpenChat = function () {
         }
 
         waitForReadyState(aRoom);
-      })["catch"](reject);
+      }).catch(reject);
     }
   });
 };
@@ -19621,7 +19625,7 @@ Chat.prototype.getEmojiDataSet = function (name) {
       self._emojiData[name] = data;
       delete self._emojiDataLoading[name];
       promise.resolve(data);
-    })["catch"](function (ev, error) {
+    }).catch(function (ev, error) {
       if (d) {
         self.logger.warn('Failed to load emoji data "%s": %s', name, error, [ev]);
       }
@@ -19777,7 +19781,7 @@ Chat.prototype.openChatAndSendFilesDialog = function (user_handle) {
   this.smartOpenChat(user_handle).then(function (room) {
     room.setActive();
     $(room).trigger('openSendFilesDialog');
-  })["catch"](this.logger.error.bind(this.logger));
+  }).catch(this.logger.error.bind(this.logger));
 };
 /**
  * Wrapper around Chat.openChat and ChatRoom.attachNodes as a single helper function
@@ -19804,8 +19808,8 @@ Chat.prototype.openChatAndAttachNodes = function (targets, nodes) {
     var attachNodes = function attachNodes(roomId) {
       return new MegaPromise(function (resolve, reject) {
         self.smartOpenChat(roomId).then(function (room) {
-          room.attachNodes(nodes).then(resolve.bind(self, room))["catch"](reject);
-        })["catch"](function (ex) {
+          room.attachNodes(nodes).then(resolve.bind(self, room)).catch(reject);
+        }).catch(function (ex) {
           if (d) {
             self.logger.warn('Cannot openChat for %s and hence nor attach nodes to it.', roomId, ex);
           }
@@ -20702,7 +20706,7 @@ ChatRoom.prototype._retrieveTurnServerFromLoadBalancer = function (timeout) {
     }
 
     $promise.resolve();
-  })["catch"](function () {
+  }).catch(function () {
     $promise.reject.apply($promise, arguments);
   });
   return $promise;
@@ -21518,10 +21522,10 @@ ChatRoom.prototype.attachNodes = function (ids) {
           } else {
             proxyPromise.reject();
           }
-        })["catch"](function (err) {
+        }).catch(function (err) {
           proxyPromise.reject(err);
         });
-      })["catch"](function (err) {
+      }).catch(function (err) {
         proxyPromise.reject(err);
       });
     } else {
@@ -21541,7 +21545,7 @@ ChatRoom.prototype.attachNodes = function (ids) {
 
         self.sendMessage(Message.MANAGEMENT_MESSAGE_TYPES.MANAGEMENT + Message.MANAGEMENT_MESSAGE_TYPES.ATTACHMENT + JSON.stringify(nodesMeta));
         proxyPromise.resolve([nodeId]);
-      })["catch"](function (r) {
+      }).catch(function (r) {
         proxyPromise.reject(r);
       });
     }

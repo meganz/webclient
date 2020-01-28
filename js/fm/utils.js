3c5007a3773a980402e0b6662dbf4363823cdc51
Khaled Daifallah <kd@mega.co.nz>
2020-01-28 14:18:50 +1300

    #14202: Feature: Check redirections from MEGAsync to warn users if needed

--- js/fm/utils.js
+++ js/fm/utils.js
@@ -1392,16 +1392,15 @@ MegaUtils.prototype.transferFromMegaCoNz = function(data) {
             // If the user is already logged in but with a different account just load that account instead. The
             // hash they came from e.g. a folder link may not be valid for this account so just load the file manager.
             else if (u_k && (JSON.stringify(u_k) !== JSON.stringify(urlParts[0]))) {
+                // if user click MEGAsync pro upgrade button and logged in as different account on webclient.
+                later(msgDialog.bind(null, 'warninga', l[882], l[19341]));
+
                 if (!urlParts[2] || String(urlParts[2]).match(/^fm/)) {
                     loadSubPage('fm');
                     return false;
                 }
                 else {
                     loadSubPage(toPage);
-                    // if user click MEGAsync pro upgrade button and logged in as different account on webclient.
-                    if (String(urlParts[2]).startsWith('pro')) {
-                        later(msgDialog.bind(null, 'warninga', l[882], l[19341]));
-                    }
                     return false;
                 }
             }

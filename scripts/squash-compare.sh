c902955a6a5b648cfab66ff4a2b05dd26a814798
Diego Casorran <dc@mega.nz>
2020-02-17 14:41:44 +0100

    Adding --smart option.

--- scripts/squash-compare.sh
+++ scripts/squash-compare.sh
@@ -3,18 +3,26 @@
 # This script is intended to see the changes between two squashed commits.
 #
 # Usage: scripts/squash-compare.sh <old-commit> <new-commit>
+#    OR: scripts/squash-compare.sh --smart [branch]
 #
 
 file1=$(mktemp)
 file2=$(mktemp)
 
-git diff develop...$1 >"$file1"
-git diff develop...$2 >"$file2"
+prev=$1
+last=$2
+
+if [[ "$1" = "--smart" ]]; then
+    [[ ! -z "$2" ]] && git checkout $2
+    reflog=$(git log --reflog --oneline | grep "$(git log -1 --pretty=%B)" |  awk '{ print $1 }')
+    prev=$(echo $reflog | awk '{ print $2 }')
+    last=$(echo $reflog | awk '{ print $1 }')
+fi
+
+git diff develop...$prev >"$file1"
+git diff develop...$last >"$file2"
 
 diff -dB -u0 --color "$file1" "$file2" | grep -vE '^[-+]?@@'
-rc=$?
 
 rm "$file1"
 rm "$file2"
-
-exit $rc

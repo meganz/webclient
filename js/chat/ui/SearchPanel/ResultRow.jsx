a529192ecb062f2e17419c0265f7f09594297ff2
Vladimir Velkov <vv@mega.co.nz>
2020-04-02 18:56:35 +0300

    #15031: results highlighting based on the search string, parsing for file names;

--- js/chat/ui/SearchPanel/ResultRow.jsx
+++ js/chat/ui/SearchPanel/ResultRow.jsx
@@ -2,55 +2,104 @@ import React from 'react';
 import { TYPE, LABEL } from './ResultContainer.jsx';
 import { Avatar, ContactPresence } from '../contacts.jsx';
 
+const ROW_CLASS = `result-table-row`;
+
+// const highlight = (text, matches) => {
+//     if (matches) {
+//         let highlighted = [];
+//         let textArray = text.split(' ');
+//
+//         for (let i = 0; i < textArray.length; i++) {
+//             let word = textArray[i];
+//
+//             for (let j = 0; j < matches.length; j++) {
+//                 const match = matches[j].str;
+//                 if (word === match) {
+//                     word = `<strong>${word}</strong>`;
+//                 }
+//             }
+//
+//             highlighted = [...highlighted, word];
+//         }
+//
+//         return highlighted.join(' ');
+//     }
+//
+//     return text;
+// };
+
+const highlight = (text, matches) => {
+    if (matches) {
+        let highlighted;
+        for (let i = matches.length; i--;) {
+            const match = matches[i].str;
+            highlighted = text.replace(new RegExp(match, 'gi'), word => `<strong>${word}</strong>`);
+        }
+        return highlighted;
+    }
+    return text;
+};
+
+// ---------------------------------------------------------------------------------------------------------------------
+
 const Message = ({ result }) => {
-    const userId = result.data.userId;
+    const { data, text, matches, room } = result;
+    const summary = data.hasAttachments() ? data.getAttachmentMeta()[0].name : text;
+
     return (
         <div
-            className="result-table-row message"
-            onClick={() => loadSubPage(result.room.getRoomUrl())}>
+            className={`${ROW_CLASS} message`}
+            onClick={() => loadSubPage(room.getRoomUrl())}>
             <span className="title">
-                {nicknames.getNicknameAndName(userId)}
-                <ContactPresence contact={M.u[userId]} />
+                {nicknames.getNicknameAndName(data.userId)}
+                <ContactPresence contact={M.u[data.userId]} />
             </span>
-            <span className="summary">
-                {result.text}
-            </span>
+            <div
+                className="summary"
+                dangerouslySetInnerHTML={{ __html: highlight(summary, matches) }}>
+            </div>
             <span className="date">
-                {time2date(result.data.delay)}
+                {time2date(data.delay)}
             </span>
         </div>
     );
 };
 
-const Chat = ({ result }) => (
-    <div
-        className="result-table-row"
-        onClick={() => loadSubPage(result.room.getRoomUrl())}>
-        {/* TODO: add static DOM re: group chats avatar */}
-        <div style={{
-            float: 'left',
-            width: 36,
-            height: 36,
-            borderRadius: 200,
-            border: '3px solid #FFF',
-            background: 'tomato' }}>
+const Chat = ({ result }) => {
+    return (
+        <div
+            className={ROW_CLASS}
+            onClick={() => loadSubPage(result.room.getRoomUrl())}>
+            {/* TODO: add static DOM re: group chats avatar */}
+            <div style={{
+                float: 'left',
+                width: 36,
+                height: 36,
+                borderRadius: 200,
+                border: '3px solid #FFF',
+                background: 'tomato' }}>
+            </div>
+            <div
+                className="user-info"
+                dangerouslySetInnerHTML={{ __html: highlight(result.room.topic, result.matches) }}>
+            </div>
+            <div className="clear"></div>
         </div>
-        <div className="user-info">
-            {result.room.topic}
-        </div>
-        <div className="clear"></div>
-    </div>
-);
+    );
+};
 
 const Member = ({ result }) => {
     const contact = M.u[result.data];
     return (
         <div
-            className="result-table-row"
+            className={ROW_CLASS}
             onClick={() => loadSubPage(result.room.getRoomUrl())}>
             <Avatar contact={contact} />
             <div className="user-info">
-                {nicknames.getNicknameAndName(result.data)}
+                <span dangerouslySetInnerHTML={{
+                    __html: highlight(nicknames.getNicknameAndName(result.data), result.matches)
+                }}>
+                </span>
                 <ContactPresence contact={contact} />
             </div>
             <div className="clear"></div>
@@ -59,7 +108,7 @@ const Member = ({ result }) => {
 };
 
 const Nil = () => (
-    <div className="result-table-row nil">
+    <div className={`${ROW_CLASS} nil`}>
         <img src={`${staticpath}images/temp/search-icon.png`} alt={LABEL.NO_RESULTS} />
         <span>{LABEL.NO_RESULTS}</span>
     </div>
@@ -79,7 +128,7 @@ export const ResultRow = ({ type, result, children }) => {
             return <Nil />;
         default:
             return (
-                <div className="result-table-row">
+                <div className={ROW_CLASS}>
                     {children}
                 </div>
             );

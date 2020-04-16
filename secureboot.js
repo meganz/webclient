1885000090c0364dd602888499c09ec74a1f72a6
Diego Casorran <dc@mega.nz>
2020-04-16 10:34:12 +0200

    Strip out full clean URLs out of #jscrashes reports.

--- secureboot.js
+++ secureboot.js
@@ -1890,6 +1890,7 @@ else if (!browserUpdate) {
                 return String(s)
                     .replace(/resource:.+->\s/,'')
                     .replace(/blob:[^:\s]+/, '..')
+                    .replace(/([^'])\w+:\/\/[^\s:]+/, '$1..')
                     .replace(/\.\.:\/\/[^:\s]+/, '..')
                     .replace('chrome://mega/content','..')
                     .replace(/file:.+extensions/,'..fx')

fa03a930747f841afd84499d9e6293dfee2a28b1
Vladimir Velkov <vv@mega.co.nz>
2020-06-12 18:31:46 +0300

    #16006: added omitted bundle

--- js/chat/bundle.js
+++ js/chat/bundle.js
@@ -17013,7 +17013,6 @@ var openResult = function openResult(room, messageId, index) {
   }
 }; //
 // MessageRow
-// TODO: add documentation
 // ---------------------------------------------------------------------------------------------------------------------
 
 
@@ -17063,7 +17062,6 @@ var resultRow_MessageRow = /*#__PURE__*/function (_MegaRenderMixin) {
   return MessageRow;
 }(mixins["MegaRenderMixin"]); //
 // ChatRow
-// TODO: add documentation
 // ---------------------------------------------------------------------------------------------------------------------
 
 
@@ -17108,7 +17106,6 @@ var resultRow_ChatRow = /*#__PURE__*/function (_MegaRenderMixin2) {
   return ChatRow;
 }(mixins["MegaRenderMixin"]); //
 // MemberRow
-// TODO: add documentation
 // ---------------------------------------------------------------------------------------------------------------------
 
 
@@ -17184,13 +17181,22 @@ var resultRow_MemberRow = /*#__PURE__*/function (_MegaRenderMixin3) {
   return MemberRow;
 }(mixins["MegaRenderMixin"]);
 
-var resultRow_NilRow = function NilRow() {
+var resultRow_NilRow = function NilRow(_ref) {
+  var onSearchMessages = _ref.onSearchMessages,
+      isFirstQuery = _ref.isFirstQuery;
   return /*#__PURE__*/external_React_default.a.createElement("div", {
     className: SEARCH_ROW_CLASS
   }, /*#__PURE__*/external_React_default.a.createElement("img", {
     src: "".concat(staticpath, "images/temp/search-icon.png"),
     alt: LABEL.NO_RESULTS
-  }), /*#__PURE__*/external_React_default.a.createElement("span", null, LABEL.NO_RESULTS));
+  }), /*#__PURE__*/external_React_default.a.createElement("span", null, LABEL.NO_RESULTS), isFirstQuery &&
+  /*#__PURE__*/
+  // [...] TODO: add translation
+  // LABEL.SEARCH_MESSAGES_INLINE
+  external_React_default.a.createElement("div", {
+    className: "search-messages",
+    onClick: onSearchMessages
+  }, "Click ", /*#__PURE__*/external_React_default.a.createElement("a", null, "here"), " to search for messages"));
 }; // ---------------------------------------------------------------------------------------------------------------------
 
 
@@ -17211,7 +17217,9 @@ var resultRow_ResultRow = /*#__PURE__*/function (_MegaRenderMixin4) {
       var _this$props4 = this.props,
           type = _this$props4.type,
           result = _this$props4.result,
-          children = _this$props4.children;
+          children = _this$props4.children,
+          onSearchMessages = _this$props4.onSearchMessages,
+          isFirstQuery = _this$props4.isFirstQuery;
 
       switch (type) {
         case TYPE.MESSAGE:
@@ -17237,7 +17245,10 @@ var resultRow_ResultRow = /*#__PURE__*/function (_MegaRenderMixin4) {
           });
 
         case TYPE.NIL:
-          return /*#__PURE__*/external_React_default.a.createElement(resultRow_NilRow, null);
+          return /*#__PURE__*/external_React_default.a.createElement(resultRow_NilRow, {
+            onSearchMessages: onSearchMessages,
+            isFirstQuery: isFirstQuery
+          });
 
         default:
           return /*#__PURE__*/external_React_default.a.createElement("div", {
@@ -17298,6 +17309,7 @@ var TYPE = {
   NIL: 4
 };
 var LABEL = {
+  // [...] TODO: Add translations
   //
   // Result table & Result row
   // ------------------------------------
@@ -17305,6 +17317,8 @@ var LABEL = {
   CONTACTS_AND_CHATS: 'Contacts And Chats',
   NO_RESULTS: 'No Results',
   RECENT: 'Recent',
+  SEARCH_MESSAGES_CTA: 'Search messages',
+  SEARCH_MESSAGES_INLINE: 'Click <a>here</a> to search for messages',
   //
   // Search field status
   // ------------------------------------
@@ -17345,7 +17359,7 @@ var resultContainer_ResultContainer = /*#__PURE__*/function (_MegaRenderMixin) {
       );
     };
 
-    _this.renderResults = function (results, status) {
+    _this.renderResults = function (results, status, isFirstQuery, onSearchMessages) {
       //
       // Result table -- `Contacts and Chats` and `Messages`, incl. `No Results`
       // https://mega.nz/#!VUkHRS4L!S2fz1aQ9Y93RZe5ky75Th9zBbdudGnApNs90TNO4eG8
@@ -17354,7 +17368,9 @@ var resultContainer_ResultContainer = /*#__PURE__*/function (_MegaRenderMixin) {
       // ----------------------------------------------------------------------
       if (status === STATUS.COMPLETED && results.length < 1) {
         return /*#__PURE__*/external_React_default.a.createElement(resultTable_ResultTable, null, /*#__PURE__*/external_React_default.a.createElement(resultRow_ResultRow, {
-          type: TYPE.NIL
+          type: TYPE.NIL,
+          isFirstQuery: isFirstQuery,
+          onSearchMessages: onSearchMessages
         }));
       }
 
@@ -17399,6 +17415,17 @@ var resultContainer_ResultContainer = /*#__PURE__*/function (_MegaRenderMixin) {
           }));
         }
 
+        if (status === STATUS.COMPLETED && key === 'MESSAGES') {
+          return /*#__PURE__*/external_React_default.a.createElement(resultTable_ResultTable, table.props, /*#__PURE__*/external_React_default.a.createElement("div", {
+            className: "search-messages default-white-button",
+            style: {
+              width: '50%',
+              margin: '10px auto'
+            },
+            onClick: onSearchMessages
+          }, LABEL.SEARCH_MESSAGES_CTA));
+        }
+
         return null;
       });
     };
@@ -17412,8 +17439,10 @@ var resultContainer_ResultContainer = /*#__PURE__*/function (_MegaRenderMixin) {
       var _this$props = this.props,
           recents = _this$props.recents,
           results = _this$props.results,
-          status = _this$props.status;
-      return recents && recents.length ? this.renderRecents(recents) : this.renderResults(results, status);
+          status = _this$props.status,
+          isFirstQuery = _this$props.isFirstQuery,
+          onSearchMessages = _this$props.onSearchMessages;
+      return recents && recents.length ? this.renderRecents(recents) : this.renderResults(results, status, isFirstQuery, onSearchMessages);
     }
   }]);
 
@@ -17576,7 +17605,7 @@ searchField_SearchField.inputRef = /*#__PURE__*/external_React_default.a.createR
 
 searchField_SearchField.select = function () {
   var inputElement = searchField_SearchField.inputRef && searchField_SearchField.inputRef.current;
-  var value = inputElement.value;
+  var value = inputElement && inputElement.value;
 
   if (inputElement && value) {
     inputElement.selectionStart = 0;
@@ -17650,6 +17679,7 @@ var searchPanel_SearchPanel = /*#__PURE__*/function (_MegaRenderMixin) {
       value: '',
       searching: false,
       status: undefined,
+      isFirstQuery: true,
       recents: [],
       results: []
     };
@@ -17690,7 +17720,7 @@ var searchPanel_SearchPanel = /*#__PURE__*/function (_MegaRenderMixin) {
 
     _this.clickedOutsideComponent = function (ev) {
       var $target = ev && $(ev.target);
-      var outsideElements = ['div.conversationsApp', 'div.fm-main', 'div.fm-left-panel', 'i.tiny-reset', 'div.small-icon.thin-search-icon'];
+      var outsideElements = ['div.conversationsApp', 'div.fm-main', 'div.fm-left-panel', 'i.tiny-reset', 'div.small-icon.thin-search-icon', 'div.search-messages, div.search-messages a'];
       return $target && // current parents !== root component
       $target.parents(".".concat(SEARCH_PANEL_CLASS)).length === 0 && // current element !== left sidebar container
       $target.parents('div.fm-left-menu.conversations').length === 0 && // current element !== left sidebar icon controls
@@ -17706,19 +17736,15 @@ var searchPanel_SearchPanel = /*#__PURE__*/function (_MegaRenderMixin) {
       _this.props.onToggle();
     };
 
-    _this.doSearch = function (s) {
-      var self = searchPanel_assertThisInitialized(_this);
-
+    _this.doSearch = function (s, searchMessages) {
       return ChatSearch.doSearch(s, function (room, result, results) {
-        self.setState({
+        return _this.setState({
           results: results
         });
-      }).catch(function (ex) {
-        if (d) {
-          console.log("Search failed (or was reset)", ex);
-        }
+      }, searchMessages).catch(function (ex) {
+        return d && console.error('Search failed (or was reset)', ex);
       }).always(function () {
-        self.setState({
+        return _this.setState({
           status: STATUS.COMPLETED
         });
       });
@@ -17766,10 +17792,11 @@ var searchPanel_SearchPanel = /*#__PURE__*/function (_MegaRenderMixin) {
         // synchronous and results might be returned quickly, we don't want to show the `IN_PROGRESS` status,
         // because `pause search` will not be available yet.
         status: value.length > 2 ? STATUS.IN_PROGRESS : undefined,
+        isFirstQuery: true,
         results: []
       }, function () {
         return searching && delay('chat-search', function () {
-          return _this.doSearch(value);
+          return _this.doSearch(value, false);
         }, 1600);
       });
     };
@@ -17802,6 +17829,18 @@ var searchPanel_SearchPanel = /*#__PURE__*/function (_MegaRenderMixin) {
           _this.doDestroy();
         }) : _this.toggleMinimize()
       );
+    };
+
+    _this.handleSearchMessages = function () {
+      return searchField_SearchField.hasValue() && _this.setState({
+        status: STATUS.IN_PROGRESS,
+        isFirstQuery: false
+      }, function () {
+        _this.doSearch(_this.state.value, true);
+
+        searchField_SearchField.focus();
+        searchField_SearchField.select();
+      });
     };
 
     return _this;
@@ -17844,6 +17883,7 @@ var searchPanel_SearchPanel = /*#__PURE__*/function (_MegaRenderMixin) {
           value = _this$state.value,
           searching = _this$state.searching,
           status = _this$state.status,
+          isFirstQuery = _this$state.isFirstQuery,
           recents = _this$state.recents,
           results = _this$state.results; //
       // `SearchPanel`
@@ -17874,7 +17914,9 @@ var searchPanel_SearchPanel = /*#__PURE__*/function (_MegaRenderMixin) {
         recents: recents
       }), searching && /*#__PURE__*/external_React_default.a.createElement(resultContainer_ResultContainer, {
         status: status,
-        results: results
+        results: results,
+        isFirstQuery: isFirstQuery,
+        onSearchMessages: this.handleSearchMessages
       }))));
     }
   }]);

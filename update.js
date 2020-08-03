a215057c800de4ee76676042280f7047bbdd7f60
Joon Shin <jsh@mega.co.nz>
2020-08-03 16:40:19 +1200

    16588: Enhancement: Send IE and old Edge to update.html

--- update.js
+++ update.js
@@ -1,64 +1,51 @@
-// If using IE 11 (https://stackoverflow.com/a/21825207)
-if (localStorage.testie11 || (Boolean(window.MSInputMethodContext) && Boolean(document.documentMode))) {
+(function() {
 
-    // Get the Continue Anyway button
-    var continueButton = document.getElementById('continue-button');
-
-    // Remove hidden style and add click handler
-    continueButton.classList.remove('hidden');
-    continueButton.addEventListener('click', function(event) {
-
-        // Prevent default button behaviour
-        event.preventDefault();
-
-        // Set flag to allow continuing to site for a small amount of time
-        localStorage.setItem('showSiteUpdateAfter', getTimeToShowPageAgain());
-
-        // Get the previous page they were on before being redirected
-        var previousPage = (localStorage.getItem('prevPage') !== null) ? '/' + localStorage.getItem('prevPage') : '';
-        var redirectPage = window.location.protocol + '//' + window.location.hostname + previousPage;
-
-        // No longer need this stored
-        localStorage.removeItem('prevPage');
-
-        // Take them back to the page they were trying to get to
-        window.location.replace(redirectPage);
-    });
-}
-
-/**
- * Get information on when to show the update page again
- * @returns {String} Returns a JSON string of an object with keys:
- *     showAgainDateTime: The timestamp in milliseconds of the earliest time when the update page can be shown again
- *     showAgainDays: The number of days that previously elapsed to arrive at the above timestamp
- */
-function getTimeToShowPageAgain() {
-    
     'use strict';
 
-    var initialDaysToShowAgain = 14;
-    var oneDayInMilliseconds = 86400 * 1000;
-    var showSiteUpdateAfter = localStorage.getItem('showSiteUpdateAfter');
+    // Support local static for testing purpose only, live should using static server.
+    if (localStorage.testie11) {
 
-    // If they've already seen the update page in the past
-    if (showSiteUpdateAfter !== null) {
+        var newStyle = document.createElement('style');
 
-        // Decode from JSON, and divide previous time in half so the update page is shown again much sooner
-        // Starts at 14 days then goes to 7, then 4 (rounded up from 3.5), then 2, then stays at 1 due to rounding
-        var updateObject = JSON.parse(showSiteUpdateAfter);
-        var showAgainDays = Math.round(updateObject.showAgainDays / 2);
+        newStyle.appendChild(document.createTextNode(
+            "@font-face {" +
+            "    font-family: 'source_sans_proregular';" +
+            "    src: url('/fonts/SourceSansPro-Regular.eot');" +
+            "    src: url('/fonts/SourceSansPro-Regular.eot?#iefix') format('embedded-opentype')," +
+            "    url('/fonts/SourceSansPro-Regular.woff2') format('woff2')," +
+            "    url('/fonts/SourceSansPro-Regular.woff') format('woff')," +
+            "    url('/fonts/SourceSansPro-Regular.ttf') format('truetype')," +
+            "    url('/fonts/SourceSansPro-Regular.svg#source_sans_proregular') format('svg');" +
+            "    font-weight: normal;" +
+            "    font-style: normal;" +
+            "}" +
+            "@font-face {" +
+            "    font-family: 'Open Sans Semibold Italic';" +
+            "    src: url('/fonts/OpenSans-SemiboldItalic_v3.eot');" +
+            "    src: url('/fonts/OpenSans-SemiboldItalic_v3.eot?#iefix') format('embedded-opentype')," +
+            "    url('/fonts/OpenSans-SemiboldItalic.woff2') format('woff2')," +
+            "    url('/fonts/OpenSans-SemiboldItalic_v3.woff') format('woff')," +
+            "    url('/fonts/OpenSans-SemiboldItalic_v3.ttf') format('truetype')," +
+            "    url('/fonts/OpenSans-SemiboldItalic_v3.svg#open_sanssemibold_italic') format('svg');" +
+            "    font-weight: normal;" +
+            "    font-style: normal;" +
+            "}"
+        ));
 
-        // Set to show again in x days
-        return JSON.stringify({
-            showAgainDateTime: (showAgainDays * oneDayInMilliseconds) + Date.now(),
-            showAgainDays: showAgainDays
-        });
+        document.head.appendChild(newStyle);
+
+        var nodes = document.querySelectorAll('.logo, .browsers-notification, .browsers-logo-lnk');
+
+        for (var i = nodes.length; i--;) {
+
+            nodes[i].style.backgroundImage = 'url(\'/images/mega/browsers-page-v3.png\')';
+        }
+
+        nodes = document.querySelectorAll('.nw-bottom-copyrights, .nw-bottom-social');
+
+        for (var j = nodes.length; j--;) {
+
+            nodes[j].style.backgroundImage = 'url(\'/images/mega/bottom-sprite-ie6.png\')';
+        }
     }
-    else {
-        // Initialise to show again in 14 days
-        return JSON.stringify({
-            showAgainDateTime: (initialDaysToShowAgain * oneDayInMilliseconds) + Date.now(),
-            showAgainDays: initialDaysToShowAgain
-        });
-    }
-}
+})();

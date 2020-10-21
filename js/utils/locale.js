06ea3894b296d013c860a6822b73ca8dce7d1382
Andrei Dymovich <ad@mega.co.nz>
2020-10-21 15:47:48 +0300

    DSN-160: Security page update

--- js/utils/locale.js
+++ js/utils/locale.js
@@ -1233,6 +1233,8 @@ mBroadcaster.once('boot_done', function populate_l() {
         .replace('[A3]', '<a href="" class="red a3">').replace('[/A3]', '</a>');
     l[23449] = escapeHTML(l[23449]).replace(/\[R\/]/g, '<sup>&reg;</sup>');
     l[24074] = escapeHTML(l[24074]).replace('[A]', '<a>').replace('[/A]', '</a>');
+    l[24141] = escapeHTML(l[24141]).replace('[A]', '<a class="red" href="https://mega.nz/blog_48">')
+        .replace('[/A]', '</a>');
 
     var common = [
         15536, 16106, 16107, 16119, 16120, 16123, 16124, 16135, 16136, 16137, 16138, 16304, 16313, 16315, 16316,

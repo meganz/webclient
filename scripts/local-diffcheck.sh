fa844a5201306840aa443b7afc3c0202d2626393
Diego Casorran <dc@mega.nz>
2021-05-06 14:51:32 +0200

    Deprecate unused scripts.

--- scripts/local-diffcheck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# A helper script to run the diffcheck script just like the Jenkins runs it at merge request time.
-#
-# How to use:
-# ./scripts/local-diffcheck.sh             (Run this script from the webclient directory)
-
-scripts/diffcheck.py --jscpd origin/develop

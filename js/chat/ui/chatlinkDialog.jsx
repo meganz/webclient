b005ccc0d38aabcb97991dc0602fe7999875bb81
Vladimir Velkov <vv@mega.co.nz>
2021-07-09 11:20:16 +0300

    WEB-13844: Enhancement: Chat link dialog / Get chat link dialog - update the design of the dialog

--- js/chat/ui/chatlinkDialog.jsx
+++ js/chat/ui/chatlinkDialog.jsx
@@ -1,66 +1,56 @@
 import React from 'react';
-import {MegaRenderMixin} from './../../stores/mixins.js';
-import ModalDialogsUI  from './../../ui/modalDialogs.jsx';
-import utils  from './../../ui/utils.jsx';
+import { MegaRenderMixin } from '../../stores/mixins.js';
+import ModalDialogsUI from './../../ui/modalDialogs.jsx';
+import utils from './../../ui/utils.jsx';
 
-
-class ChatlinkDialog extends MegaRenderMixin {
+export class ChatlinkDialog extends MegaRenderMixin {
     static defaultProps = {
-        'requiresUpdateOnResize': true,
-        'disableCheckingVisibility': true,
+        requiresUpdateOnResize: true,
+        disableCheckingVisibility: true
     }
+
     constructor(props) {
         super(props);
         this.state = {
-            'link': l[5533],
+            link: l[5533] /* `Loading...` */,
             newTopic: ''
         };
-        this.onPopupDidMount = this.onPopupDidMount.bind(this);
-        this.onClose = this.onClose.bind(this);
-        this.onTopicFieldChanged = this.onTopicFieldChanged.bind(this);
-        this.onTopicFieldKeyPress = this.onTopicFieldKeyPress.bind(this);
     }
-    onPopupDidMount($node) {
+
+    onPopupDidMount = $node => {
         this.$popupNode = $node;
-    }
+    };
+
     componentWillMount() {
-        const self = this;
-        $.dialog = "group-chat-link";
-        self.retrieveChatLink();
+        this.retrieveChatLink();
     }
-    retrieveChatLink() {
-        const self = this;
-        const chatRoom = self.props.chatRoom;
+
+    retrieveChatLink = () => {
+        const { chatRoom } = this.props;
         if (!chatRoom.topic) {
-            delete self.loading;
+            delete this.loading;
             return;
         }
 
-        self.loading = chatRoom.updatePublicHandle(undefined)
-            .always(function() {
+        this.loading = chatRoom.updatePublicHandle(undefined)
+            .always(() => {
                 if (chatRoom.publicLink) {
-                    self.setState({'link': getBaseUrl() + '/' + chatRoom.publicLink});
+                    this.setState({ 'link': getBaseUrl() + '/' + chatRoom.publicLink });
                 }
                 else {
-                    self.setState({'link': l[20660]});
+                    this.setState({ link: l[20660] /* `No chat link available.` */ });
                 }
             });
-    }
-    componentWillUnmount() {
-        super.componentWillUnmount();
-        if ($.dialog === "group-chat-link") {
-            closeDialog();
-        }
-    }
+    };
+
     componentDidUpdate() {
-        const self = this;
-        const chatRoom = this.props.chatRoom;
+        const { chatRoom } = this.props;
         if (!this.loading && chatRoom.topic) {
             this.retrieveChatLink();
         }
 
         // Setup toast notification
-        this.toastTxt = l[7654];
+        this.toastTxt = l[7654] /* `1 link was copied to the clipboard` */;
 
         if (!this.$popupNode) {
             return;
@@ -69,160 +59,154 @@ class ChatlinkDialog extends MegaRenderMixin {
         const $node = this.$popupNode;
         const $copyButton = $('.copy-to-clipboard', $node);
 
-        $copyButton.rebind('click', function() {
-            copyToClipboard(self.state.link, self.toastTxt);
+        $copyButton.rebind('click', () => {
+            copyToClipboard(this.state.link, this.toastTxt);
             return false;
         });
 
         // Setup the copy to clipboard buttons
-        $('span', $copyButton).text(l[1990]);
+        $('span', $copyButton).text(l[1990] /* `Copy` */);
     }
-    onClose() {
+
+    onClose = () => {
         if (this.props.onClose) {
             this.props.onClose();
         }
-    }
-    onTopicFieldChanged(e) {
-        this.setState({'newTopic': e.target.value});
-    }
-    onTopicFieldKeyPress(e) {
-        const self = this;
-        if (e.which === 13) {
-           self.props.chatRoom.setRoomTitle(self.state.newTopic);
-        }
-    }
-    render() {
-        const self = this;
+    };
 
-        const closeButton = (
+    onTopicFieldChanged = e => {
+        this.setState({ newTopic: e.target.value });
+    };
+
+    onTopicFieldKeyPress = e => {
+        if (e.which === 13) {
+            this.props.chatRoom.setRoomTitle(this.state.newTopic);
+        }
+    };
+
+    render() {
+        const { chatRoom } = this.props;
+        const { newTopic, link } = this.state;
+        const closeButton =
             <button
                 key="close"
-                className={"mega-button negative links-button"}
-                onClick={function(e) {
-                    self.onClose();
-                }}
-            >
-                <span>{l[148]}</span>
-            </button>
-        );
+                className="mega-button negative links-button"
+                onClick={this.onClose}>
+                <span>{l[148] /* `Close` */}</span>
+            </button>;
 
-        return <ModalDialogsUI.ModalDialog
-            {...this.state}
-            title={self.props.chatRoom.iAmOperator() && !self.props.chatRoom.topic ? l[9080] : ""}
-            className={
-                "chat-rename-dialog export-chat-links-dialog group-chat-link" + (
-                    !self.props.chatRoom.topic ? " requires-topic" : ""
-                )}
-            onClose={() => {
-                self.onClose(self);
-            }}
-            dialogName="chat-link-dialog"
-            dialogType={self.props.chatRoom.iAmOperator() && !self.props.chatRoom.topic ? 'main' : 'graphic'}
-            chatRoom={self.props.chatRoom}
-            popupDidMount={self.onPopupDidMount}>
-
-            {
-                self.props.chatRoom.iAmOperator() && !self.props.chatRoom.topic ?
+        return (
+            <ModalDialogsUI.ModalDialog
+                {...this.state}
+                title={chatRoom.iAmOperator() && !chatRoom.topic ? l[9080] /* `Rename Group` */ : ''}
+                className={`
+                    chat-rename-dialog
+                    export-chat-links-dialog
+                    group-chat-link
+                    ${chatRoom.topic ? '' : 'requires-topic'}
+                `}
+                onClose={this.onClose}
+                dialogName="chat-link-dialog"
+                dialogType={chatRoom.iAmOperator() && !chatRoom.topic ? 'main' : 'graphic'}
+                chatRoom={chatRoom}
+                popupDidMount={this.onPopupDidMount}>
+                {chatRoom.iAmOperator() && !chatRoom.topic ?
                     <section className="content">
                         <div className="content-block">
-                            <div className="export-chat-ink-warning">
-                                {l[20617]}
-                            </div>
-                            <div className="rename-input-bl" style={{
-                                width: '320px',
-                                margin: '10px auto 20px auto'
-                            }}>
-                                <input type="text" name="newTopic" value={self.state.newTopic}
-                                    ref={function(field) {
-                                        self.topicInput = field;
+                            {/* `Before you can generate a link for this chat, you need to set a description:` */}
+                            <div className="export-chat-ink-warning">{l[20617]}</div>
+                            <div
+                                className="rename-input-bl"
+                                style={{ margin: '10px auto 20px auto' }}>
+                                <input
+                                    type="text"
+                                    name="newTopic"
+                                    value={newTopic}
+                                    ref={field => {
+                                        this.topicInput = field;
                                     }}
-                                    style={{
-                                        'paddingLeft': 8,
-                                    }}
-                                    onChange={self.onTopicFieldChanged.bind(self)}
-                                    onKeyPress={self.onTopicFieldKeyPress.bind(self)}
-                                    placeholder={l[20616]} maxLength="30"/>
+                                    style={{ paddingLeft: 8, }}
+                                    onChange={this.onTopicFieldChanged}
+                                    onKeyPress={this.onTopicFieldKeyPress}
+                                    placeholder={l[20616] /* `Add a description for this chat` */}
+                                    maxLength="30"
+                                />
                             </div>
                         </div>
-                    </section>
-                    :
+                    </section> :
                     <>
                         <header>
+                            <i className="sprite-fm-uni icon-chat-group"/>
                             <h2 id="chat-link-dialog-title">
-                                <utils.EmojiFormattedContent>
-                                    {self.props.chatRoom.topic}
-                                </utils.EmojiFormattedContent>
+                                <utils.EmojiFormattedContent>{chatRoom.topic}</utils.EmojiFormattedContent>
                             </h2>
                         </header>
                         <section className="content">
                             <div className="content-block">
-
                                 <div className="chat-link-input">
-                                    <i className="sprite-fm-mono icon-link-filled" />
-                                    <input type="text" readOnly={true} value={
-                                        !self.props.chatRoom.topic ?
-                                            l[20660] :
-                                            self.state.link
-                                    }/>
+                                    <i className="sprite-fm-mono icon-link-small"/>
+                                    <input
+                                        type="text"
+                                        readOnly={true}
+                                        value={!chatRoom.topic ? l[20660] /* `No chat link available.` */ : link}
+                                    />
                                 </div>
-                                <div className="info">
-                                    {self.props.chatRoom.publicLink ? l[20644] : null}
-                                </div>
+                                {/* `People can join your group by using this link.` */}
+                                <div className="info">{chatRoom.publicLink ? l[20644] : null}</div>
                             </div>
                         </section>
                     </>
-            }
+                }
 
-            <footer>
-                <div className="footer-container">
-                    {
-                        self.props.chatRoom.iAmOperator() && self.props.chatRoom.publicLink ?
-                            <button key="deleteLink" className={"mega-button links-button" + (
-                                (
-                                    self.loading && self.loading.state() === 'pending'
-                                ) ? " disabled" : ""
-                            )} onClick={function() {
-                                self.props.chatRoom.updatePublicHandle(1);
-                                self.onClose();
-                            }}>
-                                <span>{l[20487]}</span>
+                <footer>
+                    <div className="footer-container">
+                        {chatRoom.iAmOperator() && chatRoom.publicLink &&
+                            <button
+                                key="deleteLink"
+                                className={`
+                                    mega-button
+                                    links-button
+                                    ${this.loading && this.loading.state() === 'pending' ? 'disabled' : ''}
+                                `}
+                                onClick={() => {
+                                    chatRoom.updatePublicHandle(1);
+                                    this.onClose();
+                                }}>
+                                <span>{l[20487] /* `Delete chat link` */}</span>
                             </button>
-                            :
-                            null
-                    }
-                    {
-                        self.props.chatRoom.topic ?
-                            (
-                                self.props.chatRoom.publicLink ?
-                                    <button className={"mega-button positive copy-to-clipboard" +
-                                    (self.loading && self.loading.state() === 'pending' ? " disabled" : "")}>
-                                        <span>{l[63]}</span>
-                                    </button>
-                                    :
-                                    closeButton
-                            )
-                            :
-                            (
-                                self.props.chatRoom.iAmOperator() ?
-                                    <button key="setTopic" className={"mega-button positive links-button" + (
-                                        self.state.newTopic && $.trim(self.state.newTopic) ? "" : " disabled"
-                                    )} onClick={function() {
-                                        if (self.props.chatRoom.iAmOperator()) {
-                                            self.props.chatRoom.setRoomTitle(self.state.newTopic);
-                                        }
-                                    }}>
-                                        <span>{l[20615]}</span>
-                                    </button>
-                                    :
-                                    closeButton
-                            )
-                    }
-                </div>
-            </footer>
-        </ModalDialogsUI.ModalDialog>;
-    }
-};
+                        }
 
-export {
-    ChatlinkDialog
-};
+                        {chatRoom.topic ?
+                            chatRoom.publicLink ?
+                                <button
+                                    className={`
+                                        mega-button
+                                        positive
+                                        copy-to-clipboard
+                                        ${this.loading && this.loading.state() === 'pending' ? 'disabled' : ''}
+                                    `}>
+                                    <span>{l[63] /* `Copy` */}</span>
+                                </button> :
+                                closeButton :
+                            chatRoom.iAmOperator() ?
+                                <button
+                                    key="setTopic"
+                                    className={`
+                                        mega-button
+                                        positive
+                                        links-button
+                                        ${newTopic && $.trim(newTopic) ? '' : 'disabled'}
+                                    `}
+                                    onClick={() => chatRoom.iAmOperator() && chatRoom.setRoomTitle(newTopic)}>
+                                    <span>{l[20615] /* `Set description` */}</span>
+                                </button>
+                                :
+                                closeButton
+
+                        }
+                    </div>
+                </footer>
+            </ModalDialogsUI.ModalDialog>
+        );
+    }
+}

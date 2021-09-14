abc2b13db7f85da657e5c06ade09de96ec42dbcf
Ben <bf@mega.co.nz>
2021-09-15 10:50:11 +1200

    #14355: The send button on the support form in mobile web is greyed out.

--- js/mobile/mobile.support.js
+++ js/mobile/mobile.support.js
@@ -21,41 +21,10 @@ mobile.support = {
         var $page = $('.mobile.get-support');
 
         // Init functionality
-        mobile.support.initMessageKeyup($page);
         mobile.support.initSendMessageButton($page);
 
         // Show the page
         $page.removeClass('hidden');
-    },
-
-    /**
-     * Initialise the functionality for when the user is typing into the support enquiry message text area
-     * @param {Object} $page jQuery object for the page
-     */
-    initMessageKeyup: function($page) {
-
-        'use strict';
-
-        // Cache selectors
-        var $supportEnquiryMessage = $page.find('.support-text-field');
-        var $sendButton = $page.find('.send-support-message-button');
-
-        // On keyup or clicking out of the text area
-        $supportEnquiryMessage.off('keyup blur').on('keyup blur', function() {
-
-            // Trim whitespace from the ends of the email entered
-            var queryMessage = $supportEnquiryMessage.val();
-            var queryMessageTrimmed = $.trim(queryMessage);
-
-            // If the message length is below the minimum, grey out the button so it appears unclickable
-            if (queryMessageTrimmed.length < mobile.support.minimumMessageLength) {
-                $sendButton.removeClass('active');
-            }
-            else {
-                // Otherwise how the button as red/clickable
-                $sendButton.addClass('active');
-            }
-        });
     },
 
     /**
@@ -82,6 +51,11 @@ mobile.support = {
 
             // If the message length is below the minimum, don't do anything
             if (enquiryMessageTrimmed.length < mobile.support.minimumMessageLength) {
+                msgDialog(
+                    'warninga',
+                    l[7884], // Message too short
+                    l[8650] // Your message needs to be at least %d letters long.
+                );
                 return false;
             }
 

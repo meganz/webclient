5c3869dbd0669cae8a8aef1ef89ee83a6850b39d
Vladimir Velkov <vv@mega.co.nz>
2021-09-20 06:55:16 +0000

    WEB-12852: Bugfix: Chat - "Send from Cloud Drive" dialog - The preview in...

--- js/ui/jsx/fm/nodes/columns/columnNodeName.jsx
+++ js/ui/jsx/fm/nodes/columns/columnNodeName.jsx
@@ -5,54 +5,61 @@ import {GenericNodePropsComponent} from "../genericNodePropsComponent";
 export class ColumnNodeName extends GenericNodePropsComponent {
     static sortable = true;
     static id = "name";
-    static label = l[86];
+    static label = l[86] /* `Name` */;
     static megatype = "fname";
 
+    getThumbnailSrc = node => {
+        node.imgId = `preview_${node.h}`;
+        node.seen = node.seen || 1;
+        return window.noThumbURI || '';
+    };
+
     render() {
-        let {nodeAdapter} = this.props;
-        let {node, requestThumbnailCb} = nodeAdapter.props;
+        const { nodeAdapter } = this.props;
+        const { node, requestThumbnailCb } = nodeAdapter.props;
 
-        let iconClass = "transfer-filetype-icon " + (nodeAdapter.nodeProps.isFolder ? " folder " : "") + '' +
-            nodeAdapter.nodeProps.icon;
-
-        if (node.su) {
-            iconClass += ' inbound-share';
-        }
-
-        let icon = <span
-            className={iconClass}> </span>;
-
-        let src = null;
-        if ((is_image(node) || is_video(node)) && node.fa) {
-            src = thumbnails[node.h];
-            if (!src) {
-                node.imgId = "preview_" + node.h;
-
-                if (!node.seen) {
-                    node.seen = 1; // HACK
-                }
-                src = window.noThumbURI || '';
-            }
-            icon = <Tooltips.Tooltip withArrow={true} className="tooltip-handler-container" onShown={() => {
-                requestThumbnailCb(node, true);
-            }}>
-                <Tooltips.Handler className={"transfer-filetype-icon " + fileIcon(node)}> </Tooltips.Handler>
-                <Tooltips.Contents className={"img-preview"}>
-                    <div className="dropdown img-wrapper img-block" id={"preview_" + node.h}>
-                        <img alt=""
-                            className={"thumbnail-placeholder " + node.h}
-                            src={src}
-                            width="156"
-                            height="156"
-                        />
-                    </div>
-                </Tooltips.Contents>
-            </Tooltips.Tooltip>;
-        }
-
-        return <td megatype={ColumnNodeName.megatype}>
-            {icon}
-            <span className={"tranfer-filetype-txt"}>{nodeAdapter.nodeProps.title}</span>
-        </td>;
+        return (
+            <td megatype={ColumnNodeName.megatype}>
+                {is_image(node) || is_video(node) ?
+                    <Tooltips.Tooltip
+                        withArrow={true}
+                        className="tooltip-handler-container"
+                        onShown={() => {
+                            if (!thumbnails[node.h]) {
+                                requestThumbnailCb(node, true);
+                            }
+                        }}>
+                        <Tooltips.Handler className={`transfer-filetype-icon ${fileIcon(node)}`} />
+                        <Tooltips.Contents
+                            className="img-preview">
+                            <div className="dropdown img-wrapper img-block" id={`preview_${node.h}`}>
+                                <img
+                                    alt=""
+                                    className={`thumbnail-placeholder ${node.h}`}
+                                    src={thumbnails[node.h] || this.getThumbnailSrc(node)}
+                                    width="156"
+                                    height="156"
+                                    onLoad={() => {
+                                        if (thumbnails[node.h]) {
+                                            // Trigger force update after `fm_thumbnail_render` had set the
+                                            // `src` attribute
+                                            this.safeForceUpdate();
+                                        }
+                                    }}
+                                />
+                            </div>
+                        </Tooltips.Contents>
+                    </Tooltips.Tooltip> :
+                    <span
+                        className={`
+                            transfer-filetype-icon
+                            ${nodeAdapter.nodeProps.isFolder ? 'folder' : ''}
+                            ${nodeAdapter.nodeProps.icon}
+                            ${node.su ? 'inbound-share' : ''}
+                        `}
+                    />}
+                <span className={"tranfer-filetype-txt"}>{nodeAdapter.nodeProps.title}</span>
+            </td>
+        );
     }
 }

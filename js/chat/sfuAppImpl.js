d56f7c1e470b98139c93b0fe6b53c61f094993f8
Vladimir Velkov <vv@mega.co.nz>
2021-10-12 14:29:38 +0300

    WEB-14480: Bugfix: Call feedback message after call had ended was disabled by incident

--- js/chat/sfuAppImpl.js
+++ js/chat/sfuAppImpl.js
@@ -121,7 +121,7 @@
 
     SfuApp.prototype.onDisconnect = function(termCode, willReconnect) {
         if (!willReconnect) {
-            this.room.trigger("onCallEnd", {'callId': this.callId});
+            this.room.trigger("onCallEnd", {'callId': this.callId, 'showCallFeedback': true});
             if (this.sfuClient.termCode === 1) {
                 msgDialog('warningb', '', l[20200]);
             }

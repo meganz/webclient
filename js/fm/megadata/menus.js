6bdcb4f999db14c69d4498ec9b816f9ae40de5ea
George Zhu <gz@mega.co.nz>
2022-05-06 17:15:51 +1200

    #15096: Download submenu disappears in contexu menu after view column settings menu

--- js/fm/megadata/menus.js
+++ js/fm/megadata/menus.js
@@ -646,7 +646,11 @@ MegaData.prototype.contextMenuUI = function contextMenuUI(e, ll) {
     }
     else if (ll === 7) { // Columns selection menu
         if (M && M.columnsWidth && M.columnsWidth.cloud) {
-            var $currMenuItems = $('.files-menu.context .dropdown-item')
+            // Please be aware that have to hide all hyperlink dropdown items that are options in context menu,
+            // not including any ones under submenu with the span tag.
+            // Then filter them with the classname of visible-col-select
+            // and display correct ones based on the visible columns list.
+            var $currMenuItems = $('.files-menu.context a.dropdown-item')
                 .addClass('hidden').filter('.visible-col-select');
             for (var col in M.columnsWidth.cloud) {
                 if (M.columnsWidth.cloud[col] && M.columnsWidth.cloud[col].disabled) {

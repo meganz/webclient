7aefc8f24db85783fca10708724dc2e1bf58d0bb
Ilia Pikulev <ilp@mega.co.nz>
2022-08-08 14:37:13 +1200

    #15412: Show spinner immediately when leave a large incoming share with > 10k nodes
    Co-authored-by: Diego Casorran <dc@mega.nz>

--- js/fm/megadata/render.js
+++ js/fm/megadata/render.js
@@ -241,8 +241,6 @@ MegaData.prototype.rmSetupUI = function(u, refresh) {
 
             // From inside a shared directory e.g. #fm/INlx1Kba and the user clicks the 'Leave share' button
             $('.shared-details-info-block .fm-leave-share').rebind('click', function(e) {
-                loadingDialog.show();
-
                 // Get the share ID from the hash in the URL
                 var shareId = getSitePath().replace('/fm/', '');
 

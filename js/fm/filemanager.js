2441d8a8617790f4eccd0cc0ed232eae8b267de3
Diego Casorran <dc@mega.nz>
2022-11-29 20:50:47 +0100

    #15697: When in Rubbish bin, Cloud Drive tree in LHP does not expand :-(

--- js/fm/filemanager.js
+++ js/fm/filemanager.js
@@ -4083,6 +4083,8 @@ FileManager.prototype.onSectionUIOpen = function(id) {
         case 'public-links':
         case 'backups':
         case 'rubbish-bin':
+            tmpId = 'cloud-drive';
+            break;
         case 'affiliate':
             tmpId = 'dashboard';
             break;

4038f4cdf839096f6bd1305d49ab17466e220dda
jmh <jmh@mega.co.nz>
2023-04-14 19:37:28 +1200

    WEB-26072: 20820 and 8683 need to be pluralised

--- js/transfers/zip64.js
+++ js/transfers/zip64.js
@@ -630,7 +630,8 @@ ZipWriter.prototype._eof = function() {
         return false;
     }
     else if (this.eblocked) {
-        msg = l[20820].replace('%1', this.nfiles - this.eblocked).replace('%2', this.nfiles);
+        msg = mega.icu.format(l[20820],  this.nfiles - this.eblocked)
+            .replace('%1', this.nfiles);
         msgDialog('warninga', 'Warning', escapeHTML(msg));
     }
 

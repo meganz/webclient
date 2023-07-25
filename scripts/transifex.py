ae289393ae13e3a4d7df4b52f8a8389f621719d6
Ben <bf@mega.co.nz>
2023-07-26 11:51:20 +1200

    #16340: Bugfix: Fix transifex script string deletion.

--- scripts/transifex.py
+++ scripts/transifex.py
@@ -457,7 +457,7 @@ def string_validation(new_strings):
         elif re.sub('\s', '', data['string']) == '' and re.sub('\s', '', data['developer_comment']) != '':
             print('ERROR: String with key {} has no string content'.format(key))
             valid_strings = False
-        elif 'hotfix' not in branch_name.lower() and re.search("^[A-Z]{2,4}-\d+:", data['developer_comment']) is None:
+        elif len(data['developer_comment']) and 'hotfix' not in branch_name.lower() and re.search("^[A-Z]{2,4}-\d+:", data['developer_comment']) is None:
             print('ERROR: Developer comment for string with key {} does not start with a JIRA ticket id e.g: WEB-16334: Comment content'.format(key))
             valid_strings = False
         else:

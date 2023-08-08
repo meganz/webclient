2019d9810c728105f7371f42fc723dd471811580
Jiayou Yao <jya@mega.co.nz>
2023-08-09 03:36:45 +1200

    Albums list gets duplicated and GIF link moves down in the sidebar when add or delete a picture onto Cloud drive

--- js/fm/albums/Albums.js
+++ js/fm/albums/Albums.js
@@ -4537,6 +4537,10 @@ lazy(mega.gallery, 'albums', () => {
                 }
             }
 
+            if (this.tree && this.tree.treeList.children.length > 0) {
+                this.tree.clear();
+            }
+
             this.setPredefinedAlbums(nodesArr);
             await this.setUserAlbums();
 

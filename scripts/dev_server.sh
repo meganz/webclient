3048549c281de8f1cf247fc7e69e26a96519e577
Vladimir Velkov <vv@mega.co.nz>
2024-02-21 14:41:49 +0200

    updated `dev_server.sh` re: deprecated `https` option

--- scripts/dev_server.sh
+++ scripts/dev_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ./node_modules/.bin/sass --load-path="css/" --watch css/chat-bundle.scss css/chat-bundle.css &
-NODE_ENV="development" ./node_modules/.bin/webpack-dev-server --https --config webpack.config.js
+NODE_ENV="development" ./node_modules/.bin/webpack-dev-server --server-type https --config webpack.config.js

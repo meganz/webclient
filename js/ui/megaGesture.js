55338177efc9ffe42392f854d90e43fd9a70f19e
Andrei Dymovich <ad@mega.co.nz>
2024-04-09 16:54:38 +0300

    #16822: Mobile Impossible to use scrolling together with vertical swipe gestures
    Co-authored-by: Joon Shin <jsh@mega.co.nz>

--- js/ui/megaGesture.js
+++ js/ui/megaGesture.js
@@ -7,9 +7,9 @@ class MegaGesture {
         this.minSwipeDistanceX = options.minSwipeDistanceX || 250 / window.devicePixelRatio;
         this.minSwipeDistanceY = options.minSwipeDistanceY || 250 / window.devicePixelRatio;
 
-        const _rgOpt = {capture: true};
-        const _registerGesture = () => {
-            document.body.removeEventListener('touchstart', _registerGesture, _rgOpt);
+        this._rgOpt = {capture: true};
+        this._registerGesture = () => {
+            document.body.removeEventListener('touchstart', this._registerGesture, this._rgOpt);
 
             if (!this.domNode || !this.domNode.megaGesture) {
 
@@ -26,7 +26,7 @@ class MegaGesture {
                 }
             }
         };
-        document.body.addEventListener('touchstart', _registerGesture, _rgOpt);
+        document.body.addEventListener('touchstart', this._registerGesture, this._rgOpt);
     }
 
     // This is the handleEvent method, it will be called for each event listener
@@ -59,7 +59,72 @@ class MegaGesture {
 
         this.xStart = touch.clientX;
         this.yStart = touch.clientY;
+
+        // Set real scroll nodes if `options.scrollNodes` is passed
+        this.activeScroll = this._getActiveScrollNodes(ev);
     }
+
+    _getActiveScrollNodes(ev) {
+
+        if (!this.domNode || typeof this.options.scrollNodes !== 'object'
+            || !(this.options.scrollNodes.x || this.options.scrollNodes.y)) {
+            return false;
+        }
+
+        const xScrollNodes = [].concat(this.options.scrollNodes.x || []);
+        const yScrollNodes = [].concat(this.options.scrollNodes.y || []);
+
+        // Get real scrollable area in flexbox layout mode
+        const _getScrollableArea = (scrollNodeX, scrollNodeY, node, res) => {
+
+            if (typeof node !== 'object') {
+                return false;
+            }
+
+            res = res || Object.create(null);
+
+            // Return X scroll node If scrolling exists and the node is not "inline"
+            if (scrollNodeX && node.scrollWidth > node.clientWidth && node.clientWidth !== 0) {
+                res.x = node;
+            }
+
+            // Return y scroll node If scrolling exists and the node is not "inline"
+            if (scrollNodeY && node.scrollHeight > node.clientHeight && node.clientHeight !== 0) {
+                res.y = node;
+            }
+
+            // We got all we want let's get out of here
+            if ((!!scrollNodeX === !!res.x || scrollNodeX === node)
+                && (!!scrollNodeY === !!res.y || scrollNodeY === node)) {
+                return res;
+            }
+
+            // Check parent node for scrolling
+            return _getScrollableArea(scrollNodeX, scrollNodeY, node.parentNode, res);
+        };
+
+        let xNode;
+        let yNode;
+
+        // Set real active X scroll node
+        for (var i = xScrollNodes.length; i--;) {
+            if (xScrollNodes[i].contains(ev.target)) {
+                xNode = xScrollNodes[i];
+                break;
+            }
+        }
+
+        // Set real active Y scroll node
+        for (var j = yScrollNodes.length; j--;) {
+            if (yScrollNodes[j].contains(ev.target)) {
+                yNode = yScrollNodes[j];
+                break;
+            }
+        }
+
+        return _getScrollableArea(xNode, yNode, ev.target);
+    }
+
 
     _handleTwoTouchStart(ev) {
 
@@ -87,8 +152,43 @@ class MegaGesture {
 
     _handleOneTouchMove(ev) {
 
-        if (typeof this.options.onTouchMove === 'function') {
+        this._handleScrollEvent(ev);
+
+        if (!this.action && typeof this.options.onTouchMove === 'function') {
             this.options.onTouchMove(ev);
+        }
+    }
+
+    _handleScrollEvent(ev) {
+
+        if (this.action || !(this.activeScroll.x || this.activeScroll.y)) {
+            return false;
+        }
+
+        const touch = ev.changedTouches && ev.changedTouches[0] || false;
+        let diff = 0;
+        let clientSize = 0;
+        let scrollStart = 0;
+        let scrollSize = 0;
+
+        if (this.activeScroll.x) {
+            diff = this.xStart - touch.clientX;
+            clientSize = this.activeScroll.x.clientWidth;
+            scrollStart = this.activeScroll.x.scrollLeft;
+            scrollSize = this.activeScroll.x.scrollWidth;
+        }
+        else {
+            diff = this.yStart - touch.clientY;
+            clientSize = this.activeScroll.y.clientHeight;
+            scrollStart = this.activeScroll.y.scrollTop;
+            scrollSize = this.activeScroll.y.scrollHeight;
+        }
+
+        // Prevent swipting when scroll positions are not on block edges
+        if (scrollStart !== 0 && diff < 0
+            || scrollSize > clientSize + Math.ceil(scrollStart + 1) && diff > 0) {
+
+            this.action = 'onScroll';
         }
     }
 
@@ -160,10 +260,13 @@ class MegaGesture {
         }
 
         this.action = null;
+        this.activeScroll = null;
         this.touchDistance = 0;
     }
 
     destroy() {
+        // Clear first init touchstart listener if megaGeture has not been fired previously
+        document.body.removeEventListener('touchstart', this._registerGesture, this._rgOpt);
 
         if (this.domNode) {
             this.domNode.removeEventListener('touchstart', this);

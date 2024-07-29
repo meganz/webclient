1e516108c88bd059379f6766ed1a0e20cce5c06f
Andrei Dymovich <ad@mega.co.nz>
2024-07-29 13:55:53 +0300

    #17006: Mobile loading desktop UI functions, missing

--- js/fm/linkinfohelper.js
+++ js/fm/linkinfohelper.js
@@ -187,6 +187,7 @@ LinkInfoHelper.prototype.retrieve = async function() {
         if (window.is_chatlink && is_chatlink.pnh === this.node_handle) {
             this.info.ncm = is_chatlink.ncm;
             this.info.ct = is_chatlink.ct;
+            this.info.mr = is_chatlink.mr;
             this._promise = Promise.resolve();
             return ready();
         }

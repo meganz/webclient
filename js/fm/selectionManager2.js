eccfbd1d3b291d0f17e1bee29621695828b5a75e
Ben <bf@mega.co.nz>
2025-03-27 13:43:21 +1300

    #17777: Bugfix: Move icon is displayed in Incoming shares root folder

--- js/fm/selectionManager2.js
+++ js/fm/selectionManager2.js
@@ -1289,7 +1289,7 @@ class SelectionManager2_DOM extends SelectionManager2Base {
                     if ($.selected.length === 1) {
                         __showBtn('rename');
                     }
-                    if ($.selected.length > 1) {
+                    if ($.selected.length > 1 && M.currentdirid !== 'shares') {
                         __showBtn('move');
                     }
                 }

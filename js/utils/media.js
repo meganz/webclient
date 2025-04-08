84ddfc60120dee62d32ec455329dc2eb64c1001c
Joon Shin <jsh@mega.co.nz>
2025-04-08 15:17:40 +1200

    #17791: Video stream with no sound - tooltip formatting suboptimal

--- js/utils/media.js
+++ js/utils/media.js
@@ -1224,13 +1224,14 @@ FullScreenManager.prototype.enterFullscreen = function() {
             }
             // Mute button
             else if (type === 'mute') {
+                const $muteTooltip = $('.vol-wrapper .tooltip', $wrapper).removeClass('no-sound');
                 if (videoElement.muted) {
                     $('i', $mute).removeClass().addClass(`${SPRITE} icon-volume-x-small-regular-outline`);
-                    $('.vol-wrapper .tooltip', $wrapper).text(l.video_player_unmute);
+                    $muteTooltip.text(l.video_player_unmute);
                 }
                 else {
                     $('i', $mute).removeClass().addClass(`${SPRITE} ${volumeIcon(streamer.volume)}`);
-                    $('.vol-wrapper .tooltip', $wrapper).text(l.video_player_mute);
+                    $muteTooltip.text(l.video_player_mute);
                 }
             }
         };
@@ -1480,7 +1481,7 @@ FullScreenManager.prototype.enterFullscreen = function() {
                             title = escapeHTML(l[19060]).replace('%1', streamer.hasUnsupportedAudio);
                         }
                         $('i', $vc).removeClass().addClass(`${SPRITE} icon-volume-x-small-regular-outline`);
-                        $('.vol-wrapper .tooltip', $wrapper).text(title);
+                        $('.vol-wrapper .tooltip', $wrapper).addClass('no-sound').text(title);
                     }
 
                     streamer._megaNode = node;

9b431105814d31b583e4d1137adba3535e202c38
Lawrence Williams <lw@mega.co.nz>
2025-05-15 17:33:20 +1200

    WEB-18041: Hotfix: Mobile no payment card shown

--- js/mobile/settings/account.js
+++ js/mobile/settings/account.js
@@ -412,7 +412,7 @@ mobile.settings.account = Object.create(mobile.settingsHelper, {
                 // Check if we should show the Payment Card button (uq response)
                 if (payCardBtn && mobile.settings.account.paymentCard.validateUser(M.account)) {
                     api.req({a: 'cci', v: 2})
-                        .then(mobile.settings.account.paymentCard.validateCardResponse)
+                        .then(({result}) => mobile.settings.account.paymentCard.validateCardResponse(result))
                         .then((res) => {
                             if (res) {
                                 payCardBtn.show();

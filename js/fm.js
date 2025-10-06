88db0ce1ea728ceeb08708b04c83f72fa4401d97
Rahul Dutt Sharma <rds@mega.co.nz>
2025-10-06 22:53:14 +0530

    #18396: Uncaught error when dismissing create bucket or create folder in Move or Copy dialog

--- js/fm.js
+++ js/fm.js
@@ -2167,9 +2167,8 @@ function closeDialog(ev) {
         MegaLogger.getLogger('closeDialog').debug($.dialog);
     }
 
-    if (!$('.mega-dialog.registration-page-success').hasClass('hidden')) {
-        fm_hideoverlay();
-        $('.mega-dialog.registration-page-success').addClass('hidden').removeClass('special');
+    if (mega.onCloseDialogDispatcher) {
+        tryCatch(mega.onCloseDialogDispatcher)();
     }
 
     if ($('.mega-dialog.incoming-call-dialog').is(':visible') === true || $.dialog === 'download-pre-warning') {
@@ -2370,10 +2369,12 @@ function closeDialog(ev) {
     }
 
     if ($.dialog === 'createfolder') {
+        $('.mega-dialog').trigger('dialog-closed::create-folder');
         if ($.cfpromise) {
             $.cfpromise.reject();
             delete $.cfpromise;
         }
+        delete $.cftarget;
     }
     else if ($.dialog !== 'terms') {
         delete $.mcImport;
@@ -2442,9 +2443,6 @@ function createFolderDialog(close) {
     const ltWSpaceWarning = InputFloatWarning($dialog).hide();
 
     if (close) {
-        if ($.cftarget) {
-            delete $.cftarget;
-        }
         if ($.dialog === 'createfolder') {
             closeDialog();
         }

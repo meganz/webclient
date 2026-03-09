6130ccef243ca5d856d1a6a308e819b509ddde42
Diego Casorran <dc@mega.nz>
2026-03-09 14:13:42 +0200

    #19344: disable delNodeShare() asynchronicity if not running under Lite...

--- js/fm/megadata/nodes.js
+++ js/fm/megadata/nodes.js
@@ -3975,7 +3975,7 @@ MegaData.prototype.delNodeShare = async function(h, u) {
 
     // @todo ditch MegaNode.shares use completely, maintain M.su only, adapt MegaNode.ph uses(?)
 
-    if (!(this.d[h] && this.d[h].shares)) {
+    if (!(this.d[h] && this.d[h].shares) && mega.infinity) {
         if (self.d) {
             console.assert(this.d[h] || mega.infinity, `we don't know about ${h}...yet?`, mega.infinity);
         }

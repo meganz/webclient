82b686bf9e8007a0bc08bb7f9d1bcd8e573d4707
Ben <bf@mega.co.nz>
2026-08-17 10:42:16 +1200

    #19951: Mobile Web: Intermittent - Console error Context menu node node found

--- js/mobile/mobile.context.menu.js
+++ js/mobile/mobile.context.menu.js
@@ -185,6 +185,7 @@ class MegaMobileContextMenu extends MegaComponentGroup {
 
             // duplicate the file/folder node to show it within the context menu dialog
             const itemNode = document.getElementById(this.handle)
+                || M.megaRender && M.v.some(n => n.h === this.handle) && M.megaRender.getDOMNode(this.handle)
                 || (mega.ui.viewerOverlay.visible && mega.ui.viewerOverlay.nodeComponent.domNode);
             if (!itemNode) {
                 throw new Error('Context menu node not found.');

ac6256a455c5633eb34d278c2c75a1a5175d144e
Diego Casorran <dc@mega.nz>
2026-09-09 14:31:08 +0200

    entry-points per webpack 5.110.3 ...
    Signed-off-by: Diego Casorran <dc@mega.nz>

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ var webpack = require("webpack");
 var entryPoints = [
     './js/chat/chat.jsx',
     './js/chat/ui/conversations.jsx',
+    './js/chat/chatGlobalEventManager.jsx',
+    './js/chat/ui/meetings/workflow/incoming.jsx',
 ];
 
 const path = require('path');

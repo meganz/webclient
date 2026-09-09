f8e37b41379de0f12c6c3feef0e795e0eeb86fd8
Vladimir Velkov <vladimirvelkov@gmail.com>
2026-09-09 13:59:42 +0300

    Fix `webpack-dev-server`

--- webpack.config.js
+++ webpack.config.js
@@ -37,8 +37,10 @@ var webpackConfigs = {
         devServer: {
             port: 8089,
             hot: 'only',
-            static: __dirname,
-            liveReload: false,
+            static: {
+                directory: __dirname,
+                watch: false
+            },
             webSocketServer: 'ws',
             historyApiFallback: true
         },
